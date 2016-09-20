10a9eec798fa7a714786da9c6a8eb8c7775b2e52
Aleksandar Diamond-Stanic <adiamond@bates.edu>
2016-09-20 07:49:15 -0400

    first version of bgl_aper_phot.py, designed to do simple circular aperture photometry with photutils

---
+++ bgl_aper_phot.py
@@ -0,0 +1,124 @@
+# Aleks Diamond-Stanic
+# 20160920
+#
+# the goals of this code are to do the following:
+# (1) read in three images taken in three different filters
+# (2) consider the pixels in each image that are in close proximity to the science target
+# (3) perform photometry in circular apertures
+
+# import relevant Python modules
+import os
+import numpy as np
+from astropy.io import fits
+from photutils import CircularAperture
+from photutils import CircularAnnulus
+from photutils import aperture_photometry
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# define a function to plot "postage stamp" images
+def plot_image(stamp):
+    std = np.std(stamp[stamp==stamp])
+    plt.imshow(stamp, interpolation='nearest', origin = 'lower', vmin = -1.*std, vmax = 3.*std, cmap='bone')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+
+# define the directory that contains the images
+dir = '/Users/adiamond/data/20150203_HST/J0826+4305/coarse/'
+
+# specify the position of the science target and the size of the
+# region around the science target to consider
+xcen = 3629.
+ycen = 4153.
+dx = 100.
+dy = 100.
+
+
+# create a PDF file for the plots    
+with PdfPages('bgl_aper_phot.pdf') as pdf:
+
+    fig = plt.figure()
+
+    # read in the F475W image
+    file = glob.glob(dir+'F475W/final*sci.fits')
+    hdu = fits.open(file[0])
+    data475, header475 = hdu[0].data, hdu[0].header
+    
+    # create a "postage stamp" image centered on the science target
+    stamp475 = data475[round(ycen-dy):round(ycen+dy), round(xcen-dx):round(xcen+dx)]
+
+    # plot the "postage stamp"
+    ax = fig.add_subplot(2,3,1)
+    plot_image(stamp475)
+    plt.title('F475W')
+      
+    # read in the F814W imagex
+    file = glob.glob(dir+'F814W/final*sci.fits')
+    hdu = fits.open(file[0])
+    data814, header814 = hdu[0].data, hdu[0].header
+    
+    # create a "postage stamp" image centered on the science target
+    stamp814 = data814[round(ycen-dy):round(ycen+dy), round(xcen-dx):round(xcen+dx)]
+
+    # plot the "postage stamp"
+    ax = fig.add_subplot(2,3,2)
+    plot_image(stamp814)
+    plt.title('F814W')
+
+    # read in the F160W image
+    file = glob.glob(dir+'F160W/final*sci.fits')
+    hdu = fits.open(file[0])
+    data160, header160 = hdu[0].data, hdu[0].header
+    
+    # create a "postage stamp" image centered on the science target
+    stamp160 = data160[round(ycen-dy):round(ycen+dy), round(xcen-dx):round(xcen+dx)]
+
+    # plot the "postage stamp"
+    ax = fig.add_subplot(2,3,3)
+    plot_image(stamp160)
+    plt.title('F160W')    
+
+    # plot F160W photometric curve of growth
+    positions = [(xcen, ycen)]
+    radii = np.arange(dx)+1
+    flux = []
+    for radius in radii:
+        aperture = CircularAperture(positions, radius)
+        phot_table = aperture_photometry(data160, aperture)
+        flux.append(phot_table['aperture_sum'][0])
+    ax = fig.add_subplot(2,3,6)
+    ax.plot(radii, flux, 'ro')
+    plt.ylim([0,4e6])
+    plt.xlabel('aperture radius')
+
+    # plot F814W photometric curve of growth
+    positions = [(xcen, ycen)]
+    radii = np.arange(dx)+1
+    flux = []
+    for radius in radii:
+        aperture = CircularAperture(positions, radius)
+        phot_table = aperture_photometry(data814, aperture)
+        flux.append(phot_table['aperture_sum'][0])
+    ax = fig.add_subplot(2,3,5)
+    ax.plot(radii, flux, 'ro')
+    plt.ylim([0,4e6])
+    plt.xlabel('aperture radius')
+    
+    # plot F475W photometric curve of growth
+    positions = [(xcen, ycen)]
+    radii = np.arange(dx)+1
+    flux = []
+    for radius in radii:
+        aperture = CircularAperture(positions, radius)
+        phot_table = aperture_photometry(data475, aperture)
+        flux.append(phot_table['aperture_sum'][0])
+    ax = fig.add_subplot(2,3,4)
+    ax.plot(radii, flux, 'ro')
+    plt.ylim([0,4e6])
+    plt.xlabel('aperture radius')
+    
+    pdf.savefig()
+    plt.close()
+
+    
+    os.system('open %s &' % 'bgl_aper_phot.pdf')
