ca5fb75d1244f85352eb981af20b1b8100f2ee6a
Sophia Gottlieb <sgottlie@a23368.bates.edu>
2016-10-04 13:45:08 -0400

    gau staging works, although the axes are overlapping and hard to read. needs fixing

--- sg_image_gau.py
+++ sg_image_gau.py
@@ -51,8 +51,10 @@ filename = 'sg_gau_stamp.pdf'
 with PdfPages(filename) as pdf:
     
 
-    fig = plt.figure()
+    #fig = plt.subplots()
 
+    fig = plt.figure()
+    
     y = -1
     collection = ['F475W','F814W','F160W']
     for x in collection:
@@ -79,7 +81,8 @@ with PdfPages(filename) as pdf:
         ax = fig.add_subplot(2,3,y+1)
         plot_image()
         plt.title(x)
-
+        #plt.gcf().clear()
+# begin the gau
         # select all pixels with meaningful information and put them into a
         # one-dimensional array
         pixels = data[(data==data)].ravel()
@@ -117,83 +120,18 @@ with PdfPages(filename) as pdf:
         print('sigma from gaussian fit: {0:6.3f}'.format(params_fit[2]))
         
         # visualize the histogram and the fit
+        ax = fig.add_subplot(2,3,y+4)
         plt.plot(bin_mids, hist)
         plt.plot(bin_mids[use], hist[use], color='green')
         plt.plot(bin_mids, model, color='red')
-        ax = fig.add_subplot(2,3,y+4)
-        plot_image()
+        # pull the axes to the correct scale, but doesn't flush out over the graph....
+        #plt.ylim([0,14000])
+        #plt.xlim([0,100])
+       # ax = fig.add_subplot(2,3,y+4)
+        #plot_image()
         plt.title(x)
         # plt.show()
         
-
-        pdf.savefig()
-        plt.close()
+    pdf.savefig()
+    plt.close()
 os.system('open %s &' % filename)
-
-    ####### stuff and things
-    # Aleks Diamond-Stanic
-# ft. Sophia CW Gottlieb I
-# 20160915
-#
-# written with the following goals:
-# (1) quantify the distribution of pixel values in an image
-# (2) estimate the mean and standard deviation of the sky background
-# (3) fit a Gaussian function to the background pixel values
-
-# import relevant Python modules
-
-
-#
-## read in an image file
-## dir = '/Users/adiamond/data/20150203_HST/J0826+4305/coarse/F814W/'
-## file = glob.glob(dir+'final*sci.fits')
-## dir = '/Users/sgottlie/data/test/'
-#file = glob.glob(dir+'final_F*sci.fits')
-#hdu = fits.open(file[0])
-#data, header = hdu[0].data, hdu[0].header
-#
-## select all pixels with meaningful information and put them into a
-## one-dimensional array
-#pixels = data[(data==data)].ravel()
-#
-## generate a histogram of pixel values for this array
-#bin_lo = -100
-#bin_hi = 100
-#bin_edges = np.linspace(bin_lo, bin_hi, 5000)
-#hist, edges = np.histogram(pixels, bins=bin_edges)
-#bin_mids = (bin_edges[0:-1] +  bin_edges[1:]) / 2.
-#
-## decide on range of pixel values for gaussian fit
-#rlo = -5
-#rhi = 5
-#use = (bin_mids > rlo) & (bin_mids < rhi)
-#nbin = len(use)
-#
-## estimate the errors in each bin by assuming poisson statistics: sqrt(n)
-#errors = np.sqrt(hist)
-#
-## decide on initial parameters for the gaussian
-#params_initial = [1e4, 0, 10]
-#
-## find the best-fit gaussian
-#results = minimize(objective_function, params_initial, method='Powell')
-#params_fit = results.x
-#model = gaussian(bin_mids, *params_fit)
-#
-## print the mean and median values for pixels in the image
-#print('mean pixel value in the image: ', np.mean(pixels))
-#print('median pixel value in the image: ', np.median(pixels))
-#
-## print the parameters of the best-fit gaussian function
-#print('background value from gaussian fit: {0:6.3f}'.format(params_fit[1]))
-#print('sigma from gaussian fit: {0:6.3f}'.format(params_fit[2]))
-#
-## visualize the histogram and the fit
-#plt.plot(bin_mids, hist)
-#plt.plot(bin_mids[use], hist[use], color='green')
-#plt.plot(bin_mids, model, color='red')
-#ax = fig.add_subplot(2,3,y+1)
-#plot_image()
-#plt.title(x)
-#plt.show()
-#    
