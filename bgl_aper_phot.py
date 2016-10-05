a73976632578b11db650760cadd59299d8116ce9
Aleksandar Diamond-Stanic <adiamond@bates.edu>
2016-10-05 07:34:04 -0400

    added the galaxy name to the top of the plot, added a list of more advanced goals in the comments

--- bgl_aper_phot.py
+++ bgl_aper_phot.py
@@ -1,10 +1,39 @@
 # Aleks Diamond-Stanic
-# 20160920
+# 20160920 -- 20161005
 #
-# the goals of this code are to do the following:
+# the preliminary goals of this code are to do the following:
+#
 # (1) read in three images taken in three different filters
-# (2) consider the pixels in each image that are in close proximity to the science target
+#
+# (2) consider the pixels in each image that are in close proximity to
+#     the science target
+#
 # (3) perform photometry in circular apertures
+#
+#
+# the more advanced goals include the following:
+#
+# (A) query the SDSS database for ugriz photometry for the science
+#     target
+#
+# (B) use the SDSS photometry to begin to construct an SED that will
+#     extend between 0.2 and 2 microns in the observed frame
+#
+# (C) show the profile of each filter bandpass on the SED plot
+#
+# (D) measure the total photometry in the F475W, F814W, and F160W
+#     filters for the scence target
+#
+# (E) include the three HST photometry points on the broad-band SED
+#
+# (F) construct a nuclear aperture, an intermediate annulus, and an
+#     outer annulus for the HST images
+#
+# (G) measure photometry in each of these three apertures for each of
+#     the three HST filters
+#
+# (H) construct three-band SEDs for each aperture and include these
+#     SEDs on the plot
 
 # import relevant Python modules
 import os
@@ -51,6 +80,7 @@ with PdfPages('bgl_aper_phot.pdf') as pdf:
     ax = fig.add_subplot(2,3,1)
     plot_image(stamp475)
     plt.title('F475W')
+    plt.suptitle(header475['TARGNAME'])
       
     # read in the F814W imagex
     file = glob.glob(dir+'F814W/final*sci.fits')
