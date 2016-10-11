4e88474dd018ef571b24808722117f2de21f5c98
Joshua Rines <jrines@a23369.bates.edu>
2016-10-11 09:06:32 -0400

    a few changes

---
+++ jr_aper_phot.py
@@ -0,0 +1,161 @@
+# Josh Rines
+# 20160920 -- 20161005
+#
+# the preliminary goals of this code are to do the following:
+#
+# (1) read in three images taken in three different filters
+#
+# (2) consider the pixels in each image that are in close proximity to
+#     the science target
+#
+# (3) perform photometry in circular apertures
+#
+#
+# the more advanced goals include the following:
+#
+# (A) query the SDSS database for ugriz photometry for the science
+#     target
+#
+# (B) use the SDSS photometry to begin to construct an SED that will
+#     extend between 0.2 and 2 microns in the observed frame
+#
+# (C) show the profile of each filter bandpass on the SED plot
+#
+# (D) measure the total photometry in the F475W, F814W, and F160W
+#     filters for the scence target
+#
+# (E) include the three HST photometry points on the broad-band SED
+#
+# (F) construct a nuclear aperture, an intermediate annulus, and an
+#     outer annulus for the HST images
+#
+# (G) measure photometry in each of these three apertures for each of
+#     the three HST filters
+#
+# (H) construct three-band SEDs for each aperture and include these
+#     SEDs on the plot
+
+# import relevant Python modules
+import os
+import numpy as np
+from astropy.io import fits
+from photutils import CircularAperture
+from photutils import CircularAnnulus
+from photutils import aperture_photometry
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# define a function to plot "postage stamp" images
+def plot_image(stamp):
+    std = np.std(stamp[stamp==stamp])
+    plt.imshow(stamp, interpolation='nearest', origin = 'lower', vmin = -1.*std, vmax = 3.*std, cmap='bone')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+
+# define the directory that contains the images
+dir = '/Users/jrines/data/test/'
+
+# specify the position of the science target and the size of the
+# region around the science target to consider
+xcen = 3388.
+ycen = 3504.
+dx = 100
+dy = 100
+
+
+# create a PDF file for the plots    
+with PdfPages('jr_aper_phot.pdf') as pdf:
+
+    fig = plt.figure()
+
+    # read in the F475W image
+    file = glob.glob(dir+'final_F4*sci.fits')
+    hdu = fits.open(file[0])
+    data475, header475 = hdu[0].data, hdu[0].header
+    
+    # create a "postage stamp" image centered on the science target
+    stamp475 = data475[round(ycen-dy):round(ycen+dy), round(xcen-dx):round(xcen+dx)]
+
+    # plot the "postage stamp"
+    ax = fig.add_subplot(2,3,1)
+    plot_image(stamp475)
+    plt.title('F475W')
+    plt.suptitle(header475['TARGNAME'])
+    plt.tick_params(axis='both', which='major', labelsize=8)
+      
+    # read in the F814W imagex
+    file = glob.glob(dir+'final_F8*sci.fits')
+    hdu = fits.open(file[0])
+    data814, header814 = hdu[0].data, hdu[0].header
+    
+    # create a "postage stamp" image centered on the science target
+    stamp814 = data814[round(ycen-dy):round(ycen+dy), round(xcen-dx):round(xcen+dx)]
+
+    # plot the "postage stamp"
+    ax = fig.add_subplot(2,3,2)
+    plot_image(stamp814)
+    plt.title('F814W')
+
+    # read in the F160W image
+    file = glob.glob(dir+'final_F1*sci.fits')
+    hdu = fits.open(file[0])
+    data160, header160 = hdu[0].data, hdu[0].header
+    
+    # create a "postage stamp" image centered on the science target
+    stamp160 = data160[round(ycen-dy):round(ycen+dy), round(xcen-dx):round(xcen+dx)]
+
+    # plot the "postage stamp"
+    ax = fig.add_subplot(2,3,3)
+    plot_image(stamp160)
+    plt.title('F160W')    
+
+    # plot F160W photometric curve of growth
+    positions = [(xcen, ycen)]
+    radii = np.arange(dx)+1
+    flux = []
+    for radius in radii:
+        aperture = CircularAperture(positions, radius)
+        phot_table = aperture_photometry(data160, aperture)
+        flux.append(phot_table['aperture_sum'][0])
+    ax = fig.add_subplot(2,3,6)
+    ax.plot(radii, flux, 'ro')
+    plt.ylim([0,4e6])
+    plt.xlabel('aperture radius')
+    plt.ylabel('flux')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+
+    # plot F814W photometric curve of growth
+    positions = [(xcen, ycen)]
+    radii = np.arange(dx)+1
+    flux = []
+    for radius in radii:
+        aperture = CircularAperture(positions, radius)
+        phot_table = aperture_photometry(data814, aperture)
+        flux.append(phot_table['aperture_sum'][0])
+    ax = fig.add_subplot(2,3,5)
+    ax.plot(radii, flux, 'ro')
+    plt.ylim([0,4e6])
+    plt.xlabel('aperture radius')
+    plt.ylabel('flux')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+    
+    # plot F475W photometric curve of growth
+    positions = [(xcen, ycen)]
+    radii = np.arange(dx)+1
+    flux = []
+    for radius in radii:
+        aperture = CircularAperture(positions, radius)
+        phot_table = aperture_photometry(data475, aperture)
+        flux.append(phot_table['aperture_sum'][0])
+    ax = fig.add_subplot(2,3,4)
+    ax.plot(radii, flux, 'ro')
+    plt.ylim([0,4e6])
+    plt.xlabel('aperture radius')
+    plt.ylabel('flux')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+    
+    pdf.savefig()
+    plt.close()
+
+    
+    os.system('open %s &' % 'jr_aper_phot.pdf')
