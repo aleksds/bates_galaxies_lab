4aea3fce2589461782052164e0e7144abadd9717
Sophia Gottlieb <sgottlie@a23368.bates.edu>
2016-10-14 21:04:37 -0400

    Sophia does some programming to make a Flux v filter graph on python. Flux of a radius chunk is plotted (flux[i]-flux[i-1]). possible upcoming edits: color code? unsure I've made this chart possible to connect. more science needed

---
+++ sg_flux_pix.py
@@ -0,0 +1,91 @@
+# Sophia CW Gottlieb I
+# 20161014
+#
+# the preliminary goals of this code are to do the following:
+#
+# (1) read in three images taken in three different filters
+# 
+# (2) consider the pixels in each image that are in close proximity to
+#     the science target
+#
+# (3) perform photometry in circular apertures
+#
+#
+# import relevant Python modules
+import os
+import numpy as np
+from astropy.io import fits
+from photutils import CircularAperture
+from photutils import CircularAnnulus
+from photutils import aperture_photometry
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# define a function to plot "postage stamp" images
+def plot_image(stamp):
+    std = np.std(stamp[stamp==stamp])
+    plt.imshow(stamp, interpolation='nearest', origin = 'lower', vmin = -1.*std, vmax = 3.*std, cmap='bone')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+    
+# define the directory that contains the images
+dir = '/Users/sgottlie/data/test/'
+
+# specify the position of the science target and the size of the
+# region around the science target to consider
+xcen = 3629.
+ycen = 4153.
+dx = 100.
+dy = 100.
+
+# create a PDF file for the plots    
+with PdfPages('sg_flux_pix.pdf') as pdf:
+    fig = plt.figure()
+
+    # I THINK THIS WILL DO IT
+    
+    collection = ['F475W','F814W','F160W']
+    filter = [475, 814, 1600]
+    
+    for i in range(0, len(collection)):
+        
+        # read in image
+        # how can I modify this code to work for a loop?
+        file = glob.glob(dir+'final_' + collection[i] + '*sci.fits')
+        print(file)
+        hdu = fits.open(file[0])
+        data, header = hdu[0].data, hdu[0].header
+
+        plt.suptitle(header['TARGNAME'])
+
+        #plot the thing; do the stuff
+        positions = [(xcen, ycen)]
+        radii = np.arange(dx)+1
+        wavelength = np.arange(dx)+1
+        areaflux = np.arange(dx)+1
+
+        flux = []
+        for radius in radii:
+            aperture = CircularAperture(positions, radius)
+            phot_table = aperture_photometry(data, aperture)
+            flux.append(phot_table['aperture_sum'][0])
+        ax = fig.add_subplot(1,1,1)
+        wavelength[0] = filter[i]
+        for j in range (1, len(radii)):
+            wavelength[j] = filter[i]
+            areaflux[j] = flux[j] - flux [j-1]
+        ax.plot(wavelength, areaflux, 'ro')
+        # ax.plot(wavelength, flux, 'ro')
+        # plt.ylim([0,4e6])
+        plt.ylim([0,5e5])
+        plt.xlim([400,1675])
+        plt.xlabel('wavelength in nm')
+        plt.ylabel('mother fluxer')
+
+    ## HERE IS WHERE I STOPPED FUCKING SHIT UP
+
+    
+    pdf.savefig()
+    plt.close()
+os.system('open %s &' % 'sg_flux_pix.pdf')
+
