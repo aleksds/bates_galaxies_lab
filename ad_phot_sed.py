ae6d862162d07021e49e6deb8e1fabddf208f054
Aleksandar Diamond-Stanic <adiamond@bates.edu>
2016-10-17 08:02:19 -0400

    updated ad_phot_sed.py to include colorbar scaled to aperture radius and lines connecting points from different apertures

--- ad_phot_sed.py
+++ ad_phot_sed.py
@@ -10,22 +10,20 @@ import os
 import numpy as np
 from astropy.io import fits
 from photutils import CircularAperture
-from photutils import CircularAnnulus
 from photutils import aperture_photometry
 import glob
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from matplotlib.backends.backend_pdf import PdfPages
 
-
 # define the directory that contains the images
-dir = '/Users/adiamond/data/20150203_HST/J0826+4305/coarse/'
+dir = os.environ['HSTDIR']
 
-# specify the position of the science target and the size of the
-# region around the science target to consider
+# specify the position of the science target
 xcen = 3629.
 ycen = 4153.
-#dx = 100.
-#dy = 100.
+
+# define the radii to be used for aperture photometry
 radii = np.array([1,2,3,4,5,8,10,15,20,25,30,35,40])
 
 # create a PDF file for the plots    
@@ -34,56 +32,68 @@ with PdfPages('ad_phot_sed.pdf') as pdf:
     fig = plt.figure()
 
     # read in the F475W image
-    file = glob.glob(dir+'F475W/final*sci.fits')
+    file = glob.glob(dir+'../F475W/final*sci.fits')
     hdu = fits.open(file[0])
     data475, header475 = hdu[0].data, hdu[0].header
 
-    # read in the F814W imagex
-    file = glob.glob(dir+'F814W/final*sci.fits')
+    # read in the F814W image
+    file = glob.glob(dir+'../F814W/final*sci.fits')
     hdu = fits.open(file[0])
     data814, header814 = hdu[0].data, hdu[0].header
 
     # read in the F160W image
-    file = glob.glob(dir+'F160W/final*sci.fits')
+    file = glob.glob(dir+'../F160W/final*sci.fits')
     hdu = fits.open(file[0])
     data160, header160 = hdu[0].data, hdu[0].header
 
-    ax = fig.add_subplot(1,1,1)
+    # define positions for photometry
     positions = [(xcen, ycen)]
-    # plot F475W photometric points
-    flux475 = []
+    
+    # do photometry on F475W image
+    flux475 = [0.]
     for radius in radii:
         aperture = CircularAperture(positions, radius)
         phot_table = aperture_photometry(data475, aperture)
         flux475.append(phot_table['aperture_sum'][0])
-        wv = np.array([0.,475])
-        fx = np.array([0., flux475[-1]])
-        ax.plot(wv, fx, 'ro')
-
-    # plot F814W photometric points
-    flux814 = []
+        
+    # do photometry on F814W image
+    flux814 = [0.]
     for radius in radii:
         aperture = CircularAperture(positions, radius)
         phot_table = aperture_photometry(data814, aperture)
         flux814.append(phot_table['aperture_sum'][0])
-        wv = np.array([0.,814])
-        fx = np.array([0., flux814[-1]])
-        ax.plot(wv, fx, 'ro')
         
-    # plot F160W photometric points
-    flux160 = []
+    # do photometry on F160W image
+    flux160 = [0.]
     for radius in radii:
         aperture = CircularAperture(positions, radius)
         phot_table = aperture_photometry(data160, aperture)
         flux160.append(phot_table['aperture_sum'][0])
-        wv = np.array([0.,1600])
-        fx = np.array([0., flux160[-1]])
-        ax.plot(wv, fx, 'ro')    
+
+    # set up the plot
+    ax = fig.add_subplot(1,1,1)
+
+    # plot lines connecting photometric points for each aperture
+    for i in range(0,len(radii)):
+        wv = np.array([475.,814.,1600])
+        fx = np.array([flux475[i+1]-flux475[i], flux814[i+1]-flux814[i], flux160[i+1]-flux160[i]])
+        ax.plot(wv, fx, color='0.75')
+
+    # plot symbols for each photometric point with color scaled to aperture radius
+    for i in range(0,len(radii)):
+        wv = np.array([475.,814.,1600])
+        fx = np.array([flux475[i+1]-flux475[i], flux814[i+1]-flux814[i], flux160[i+1]-flux160[i]])
+        cax = ax.scatter(wv, fx, c=np.array([radii[i],radii[i],radii[i]]), vmin=radii[0], vmax=radii[-1], cmap=cm.coolwarm, s=25, lw=0.2, marker='s')
         
     # set plot parameters
-    plt.ylim([0,4e6])
+    cbar = fig.colorbar(cax)
+    cbar.set_label('radius [pixels]', fontsize=18)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    plt.ylim([1e4,6e5])
+    plt.ylabel('flux [image units]', fontsize=18)
     plt.xlim([300.,2000.])
-    plt.xlabel('wavelength')  
+    plt.xlabel(r'wavelength [$\AA$]', fontsize=18)
 
     pdf.savefig()
     plt.close()
