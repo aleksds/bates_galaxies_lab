519dbb939107f88a2d9ea94dfc4998f8bf05a6ac
Joshua Rines <jrines@a23369.bates.edu>
2016-11-11 13:49:39 -0500

    added headers and consistent with analysis for Data Paper

--- jr_phot_sed_noloop.py
+++ jr_phot_sed_noloop.py
@@ -26,7 +26,7 @@ dx = 100
 dy = 100
 
 # define the radii to be used for aperture photometry
-radii = np.array([1,2,3,4,5,8,10,15,20,25,30,35,40])
+radii = np.array([1,2,3,4,5])
 
 # create a PDF file for the plots    
 with PdfPages('jr_phot_sed_noloop.pdf') as pdf:
@@ -86,13 +86,14 @@ with PdfPages('jr_phot_sed_noloop.pdf') as pdf:
         wv = np.array([475.,814.,1600])
         fx = np.array([flux475[i+1]-flux475[i], flux814[i+1]-flux814[i], flux160[i+1]-flux160[i]])
         cax = ax.scatter(wv, fx, c=np.array([radii[i],radii[i],radii[i]]), vmin=radii[0], vmax=radii[-1], cmap=cm.coolwarm, s=25, lw=0.2, marker='s')
+        plt.title('Flux vs. Wavelength, Inner 5 Annuli')
         
     # set plot parameters
     cbar = fig.colorbar(cax)
     cbar.set_label('radius [pixels]', fontsize=18)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    plt.ylim([1e4,6e5])
+    plt.ylim([1e3,6e7])
     plt.ylabel('flux [image units]', fontsize=18)
     plt.xlim([300.,2000.])
     plt.xlabel(r'wavelength [$\AA$]', fontsize=18)
