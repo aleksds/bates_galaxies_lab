08c3714ad6c06111e487233e8f64556377892bbd
Fahim Sakil Khan <fkhan@bates.edu>
2017-03-17 11:30:15 -0400

    Dank Code

--- bgl_dap_maps_logcube.py
+++ bgl_dap_maps_logcube.py
@@ -10,11 +10,11 @@ from mangadap.dapcube import DAPCubeBitMask
 
 dapdir = os.environ['DAPDIR']
 
-dir = dapdir+'SPX-GAU-MILESHC/7977/12704/'
+dir = dapdir+'SPX-GAU-MILESHC/7443/12704/'
 
 # Open the fits file
-hdu_maps = fits.open(dir+'manga-7977-12704-MAPS-SPX-GAU-MILESHC.fits.gz')
-hdu_cube = fits.open(dir+'manga-7977-12704-LOGCUBE-SPX-GAU-MILESHC.fits.gz')
+hdu_maps = fits.open(dir+'manga-7443-12704-MAPS-SPX-GAU-MILESHC.fits.gz')
+hdu_cube = fits.open(dir+'manga-7443-12704-LOGCUBE-SPX-GAU-MILESHC.fits.gz')
 
 # Get the S/N per bin from the MAPS file
 snr = np.ma.MaskedArray(hdu_maps['BIN_SNR'].data, mask=hdu_maps['BINID'].data < 0)
@@ -23,8 +23,8 @@ snr = np.ma.MaskedArray(hdu_maps['BIN_SNR'].data, mask=hdu_maps['BINID'].data < 
 k = np.ma.argmax(snr.ravel())
 n = hdu_maps['BIN_SNR'].data.shape[0] # Number of pixels in X and Y
 # Get the pixel coordinate
-j = k//n
-i = k - j*n
+i=38
+j=37
 
 # Declare the bitmask object to mask selected pixels
 bm = DAPCubeBitMask()
@@ -48,14 +48,47 @@ with PdfPages('dap_maps_logcube.pdf') as pdf:
 
     fig = plt.figure()
 
-    ax = fig.add_subplot(1,1,1)
+    ax = fig.add_subplot(3,1,1)
     
     ax.step(wave, flux, where='mid', color='k', lw=0.5)
     ax.plot(wave, model, color='r', lw=1)
     ax.plot(wave, stellarcontinuum, color='g', lw=1)
     ax.plot(wave, emlines, color='b', lw=1)
     ax.step(wave, resid, where='mid', color='0.5', lw=0.5)
-
+    #plt.xlim([3500,10500])
+    #plt.ylim([-0.2,0.2])
+    plt.xlabel('Wavelength [$\AA$]',fontsize=10)
+    plt.ylabel('$f_{\lambda}$ [$10^{-17}$ erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$]',fontsize=8)
+    ax.text(7000,0.15,str(i)+','+str(j))
+    pdf.savefig()
+    plt.close()
+    fig=plt.figure()
+    
+    
+    ax = fig.add_subplot(3,1,1)
+    ax.step(wave, flux, where='mid', color='k', lw=0.5)
+    ax.plot(wave, model, color='r', lw=1)
+    ax.plot(wave, stellarcontinuum, color='g', lw=1)
+    ax.plot(wave, emlines, color='b', lw=1)
+    plt.xlim([3800,3860])
+    plt.ylim([-.05,np.max(model)])
+    plt.xlabel('Wavelength [$\AA$]',fontsize=10)
+    plt.ylabel('$f_{\lambda}$ [$10^{-17}$ erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$]',fontsize=8)
+    ax.text(3835,0.08,str(i)+','+str(j))
+    
+    lambda_zero=3830
+    ax = fig.add_subplot(3,1,3)
+    vel=3E5*(wave-lambda_zero)/lambda_zero
+    ax.step(vel, flux, where='mid', color='k', lw=0.5)
+    ax.plot(vel, model, color='r', lw=1)
+    ax.plot(vel, stellarcontinuum, color='g', lw=1)
+    ax.plot(vel, emlines, color='b', lw=1)
+    plt.xlim([-1000,1000])
+    plt.ylim([-.05,np.max(model)])
+    plt.xlabel('Velocity (km s$^{-1}$)',fontsize=10)
+    plt.ylabel('$f_{\lambda}$ [$10^{-17}$ erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$]',fontsize=8)
+    ax.text(100,0.08,str(i)+','+str(j))
+    
     pdf.savefig()
     plt.close()
 
