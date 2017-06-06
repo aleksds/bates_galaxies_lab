95312430b38b248c27d6e593b4e0ebc13ea8d0b0
adiamond <adiamond@a23405.bates.edu>
2017-06-06 15:27:45 -0400

    version that includes flux and wavelength

--- hires/ad_plot_sdss.py
+++ hires/ad_plot_sdss.py
@@ -2,6 +2,7 @@
 # code to read in and plot an SDSS spectrum
 
 import os
+import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -10,15 +11,22 @@ dir = os.environ['SDSSDIR']
 file = dir + 'spec-0761-54524-0409.fits'
 hdulist = fits.open(file)
 
-plt.plot(hdulist[5].data)
+coeff0 = hdulist[0].header['COEFF0']
+coeff1 = hdulist[0].header['COEFF1']
+
+flux = hdulist[1].data
+
+npix = len(flux)
+index = np.arange(npix)
+wavelength = 10.**(coeff0 + coeff1*index)
 
 filename = 'J0826_sdss.pdf'
 with PdfPages(filename) as pdf:
 
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.plot(hdulist[5].data)
-
+    ax.plot(wavelength, flux)
+    ax.set_ylim(0., 10.)
     pdf.savefig()
     plt.close()
     
