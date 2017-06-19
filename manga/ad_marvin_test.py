62752133d3998c4adbab52bc5b22757510fea1d4
adiamond <adiamond@a23405.bates.edu>
2017-06-19 16:32:39 -0400

    version using local filename

--- manga/ad_marvin_test.py
+++ manga/ad_marvin_test.py
@@ -3,8 +3,10 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-maps = Maps(plateifu='7977-12704', mode='remote')
+#maps = Maps(plateifu='7977-12704', mode='remote')
 #maps = Maps(plateifu='7977-12704', mode='local')
+maps = Maps(plateifu='7977-12704', mode='local', filename='/usr/netapp/physics/linux-lab/data/sas/mangawork/manga/spectro/analysis/v2_0_1/SPX-GAU-MILESHC/7977/12704/manga-7977-12704-MAPS-SPX-GAU-MILESHC.fits.gz')
+#maps = Maps(plateifu='7977-12704', mode='local', data_origin='file')
 print(maps)
 # get an emission line map
 
