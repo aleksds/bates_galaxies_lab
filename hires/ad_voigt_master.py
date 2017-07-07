c929bd09d23eb07828aa322f1dddf9ccb3a71fd2
kdelva <kdelva@bates.edu>
2017-07-07 10:40:46 -0400

    Making moves

--- hires/ad_voigt_master.py
+++ hires/ad_voigt_master.py
@@ -104,7 +104,7 @@ with PdfPages(filename) as pdf:
         ax.set_xlim(-3000, 500)
         ax.set_ylim(0, 2)
         #plt.legend(loc = 1)
-        plt.title("Uncertainty of Flux in %s" %(gal[h]))
+        plt.title("Voigt Profile : Velocity vs Flux in %s" %(gal[h]))
         plt.xlabel("Velocity(km/s)")
         plt.ylabel("Continuum Normalized Flux")
         #handles, labels = ax.get_legend_handles_labels()
@@ -124,42 +124,48 @@ with PdfPages(filename) as pdf:
 
         plt.errorbar(vel_2796, col_2796, yerr = sigma_tau2796, linewidth = 0.1, color = '#99ccff', label = 'error')
         plt.errorbar(vel_2803, col_2803, yerr = sigma_tau2803, linewidth = 0.1, color = '#99ccff', label = '_nolegend_')
-        ax.plot(vel_2796, col_2796, color = '#2CA14B', label = names[0])
-        ax.plot(vel_2803, col_2803, color = '#2C6EA1', label = names[1])
-        plt.title("Uncertainty of Column Density in %s" %(gal[h]))
-        plt.xlabel("Velocity (km/s)")
-        plt.ylabel("Column Density (Particle/cm^2)")
-        ax.set_ylim(0, 5E12)
-        ax.set_xlim(-3000,500)
-        plt.legend(loc = 1)
+        # ax.plot(vel_2796, col_2796, color = '#2CA14B', label = names[0])
+        # ax.plot(vel_2803, col_2803, color = '#2C6EA1', label = names[1])
+        # plt.title("Uncertainty of Column Density in %s" %(gal[h]))
+        # plt.xlabel("Velocity (km/s)")
+        # #plt.ylabel("Column Density (Particle/cm^2)")
+        # ax.set_ylim(0, 5E12)
+        # ax.set_xlim(-3000,500)
+        # plt.legend(loc = 1)
 
 
         f = interp1d(vel_kms[0], flux)
         #vel_new = np.linspace(-3000, 500, num = 3501, endpoint = True)
-        vel_new = np.linspace(-2000, -800, num = 1201, endpoint = True)
-        flux_king = f(vel_new)
+        #vel_new = np.linspace(-2000, -800, num = 1201, endpoint = True)
+        while vel_kms >= -3000 & vel_kms <= -500:
+            if flux <= .5:
+                vel_new = np.linspace(vel_kms(flux) - 600, vel_kms[0](flux)+600), num = 1201, endpoint = True)
+                # To proceed, make smart for loops that plots voigt profile based on height of line or manually look at code and plot regions where it looks like code has dip
+            else False
 
-        xarr = vel_new
-        yarr = flux_king - 1.
+                flux_king = f(vel_new)
 
-        voi_init = Voigt1D(amplitude_L=-1.0, x_0=-1300, fwhm_L=200, fwhm_G=200)
-        fitter = fitting.LevMarLSQFitter()
-        voi_fit = fitter(voi_init, xarr, yarr)
-        print(voi_fit)
+                xarr = vel_new
+                yarr = flux_king - 1.
 
-        ax = fig.add_subplot(3,1,3)
-        #ax.plot(xarr,yarr+1, color='magenta')
-        ax.plot(xarr,voi_fit(xarr)+1, color='red')
-        #ax.plot(xarr, voi_init(xarr)+1, color='orange')
-        plt.xlabel("Velocity(km/s)")
-        plt.ylabel("Continuum Normalized Flux")
-        ax.set_ylim (0,2)
-        ax.set_xlim(-3000,500)
+                voi_init = Voigt1D(amplitude_L=-1.0, x_0=-1300, fwhm_L=200, fwhm_G=200)
+                fitter = fitting.LevMarLSQFitter()
+                voi_fit = fitter(voi_init, xarr, yarr)
+                print(voi_fit)
+
+                ax = fig.add_subplot(3,1,3)
+                #ax.plot(xarr,yarr+1, color='magenta')
+                ax.plot(xarr,voi_fit(xarr)+1, color='red')
+                #ax.plot(xarr, voi_init(xarr)+1, color='orange')
+                plt.xlabel("Velocity(km/s)")
+                plt.ylabel("Continuum Normalized Flux")
+                ax.set_ylim (0,2)
+                ax.set_xlim(-3000,500)
         
-        fig.tight_layout()
-        pdf.savefig()
-        plt.close()
-os.system("evince  %s &" % 'Error_calculation.pdf')
+                fig.tight_layout()
+                pdf.savefig()
+                plt.close()
+            os.system("open  %s &" % 'Error_calculation.pdf')
 
 
 
@@ -188,148 +194,4 @@ os.system("evince  %s &" % 'Error_calculation.pdf')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##
-
-        
-# # read in the relevant SDSS spectrum
-# dir = os.environ['SDSSDIR']
-# file = dir + 'spec-0761-54524-0409.fits'
-# hdulist = fits.open(file)
-
-# # define the coefficients that are used to define the wavelength array
-# # For information about how to determine wavelength associated with each pixel: http://classic.sdss.org/dr5/products/spectra/read_spSpec.html
-# coeff0 = hdulist[0].header['COEFF0']
-# coeff1 = hdulist[0].header['COEFF1']
-
-# # define the flux and wavelength arrays
-# flux = hdulist[1].data['flux']
-# loglam = hdulist[1].data['loglam']
-# model = hdulist[1].data['model']
-# ivar = hdulist[1].data['ivar']
-# npix = len(flux)
-# index = np.arange(npix)
-
-# #To do: check whether this wavelength definition is correct and whether it's in air or vacuum
-# #wavelength = 10.**(coeff0 + coeff1*index)
-
-# #Background Information
-# wavelength = 10.**loglam
-# c = 3 * 10 **8
-# z = .7114
-
-# #Background Info Manipulation
-# rest_wave = np.array([2600, 2796])
-# obs_wave = []
-# for i in range(0,len(rest_wave)):
-#     wave_calc = (rest_wave[i] * (1+z))
-#     obs_wave = np.append(obs_wave,wave_calc)
-
-# velo_int=[]
-# for i in range(0,len(obs_wave)):
-#     velo_calc = (obs_wave - rest_wave)/(rest_wave) *c
-#     velo_int = np.append(velo_int, velo_calc)
-
-# for i in range(0, len(wavelength)):    
-#     velocity = np.array([((wavelength*(1+z)) - wavelength[i]) *c / wavelength[i]])
-
-
-
-
-
-# ## Gaussian function where alpha is the half-width at half-max(Gaussian) and x is the width
-# def Gaussian(x, alpha):
-#     return np.sqrt(np.log(2)/np.pi)/alpha *np.exp(-(x/alpha)**2*np.log(2))
-
-
-# ## Lorentzian function where gamma is the half-width at half-max (Lorentzian) and x is the width
-# def Lorentzian(x, gamma):
-#     return gamma / np.pi / (x**2 + gamma**2)
-
-# #Voigt Function
-# def Voigt(x,alpha, gamma):
-#     sigma = alpha / np.sqrt(2*np.log(2))
-#     return np.real(wofz((x+1j*gamma)/sigma/np.sqrt(2)))/sigma/np.sqrt(2*np.pi)
-
-
-
-# Random Values to see if code works
-#alpha, gamma = -200, -200
-#x = np.linspace(-0.8,0.8,1000)
-#x = np.linspace(-3000,500,3500)
-## Gotta find real values for alpha, gamma, and x with Aleks
-
-
-# graphs Gaussian, Lorentzian, and Voigt
-# pylab.plot(x, Gaussian(x, alpha), ls=':', c='k', label='Gaussian')
-# pylab.plot(x, Lorentzian(x, gamma), ls='--', c='k', label='Lorentzian')
-# pylab.plot(x, Voigt(x, alpha, gamma), c='k', label='Voigt')
-# pylab.xlim(-0.8,0.8)
-# pylab.legend()
-# pylab.show()
-
-# # create a PDF file for plot output
-# filename = 'J0905_sdss.pdf'
-# with PdfPages(filename) as pdf:
-    
-    # fig = plt.figure()
-
-
-#From ad_voigt Test
-
-
-
-##
-
-
-    
-    # ax.plot(xarr,yarr)
-    # ax.plot(xarr,voi_fit(xarr))
-
-    # ax = fig.add_subplot(4,1,1)
-    # ax.plot(x, Gaussian(x, alpha), ls=':', c='k', label='Gaussian')
-    # ax.set_xlim(-3000,500)
-    # plt.legend(loc=2)
-
-    # ax = fig.add_subplot(4,1,2)
-    # ax.plot(x, Lorentzian(x, gamma), ls='--', c='k', label='Lorentzian')
-    # ax.set_xlim(-3000,500)
-    # plt.legend(loc=2)
-
-    # ax = fig.add_subplot(4,1,3)
-    # ax.plot(x, Voigt(x, alpha, gamma), c='k', label='Voigt')
-    # ax.set_xlim(-3000,500)
-    # plt.legend(loc=2)
-
-    # ax = fig.add_subplot(4,1,4)
-    # ax.plot(x, Gaussian(x, alpha), ls=':', c='k', label='Gaussian')
-    # ax.plot(x, Lorentzian(x, gamma), ls='--', c='k', label='Lorentzian')
-    # ax.plot(x, Voigt(x, alpha, gamma), c='k', label='Voigt')
-    # ax.set_xlim(-3000,500)
-    # plt.legend(loc=2)
-
-#     fig.tight_layout()
-#     # save the figure
-#     pdf.savefig()
-#     plt.close()
-    
-#     # open the PDF file
-# os.system("open %s &" % filename)
 
