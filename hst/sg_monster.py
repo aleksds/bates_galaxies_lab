fde5bc20de3252fb6383aaa106a8ec6a218e116a
ashley warner <aaw@ashley-warners-computer.local>
2017-07-10 19:55:51 -0400

    currently colorplots mass for snr restricted areas of each filter. it does not limit all work to 475nm thought maybe it should

--- hst/sg_monster.py
+++ hst/sg_monster.py
@@ -7,6 +7,8 @@
 # Then, SNR for pixel
 # Then, MLR for significant pixels
 # But to begin, we will do SNR and MLR because those are mine....
+# currently does SNR and MLR in pixel mode....
+# need to talk to aleks about SNR restrictions via filter vs color....
 
 #import relavent packages
 import os
@@ -29,9 +31,35 @@ from astropy import units as u
 from astropy import constants as const
 from astropy.cosmology import FlatLambdaCDM 
 
+# flux makes magnitude of a given flux in janskys.
+def mag(val):
+    return -2.5*np.log10(val/3631)
+
+# flux makes luminosity defined by Hogg 1999??
+def luminosity(wavelength,flux,lDcm):
+    return const.c*u.s/u.m/(wavelength*10**-9)*flux*10**-23*(4*math.pi*lDcm**2)/solarLum
+
+# Given two coefficients and the color will return the mass-to-light ratio
+# as defined by Bell and DeJong 2000
+def mLR(a,b,color):
+    return 10**(a+b*color)
+
+
 # define the directory that contains the images
-#dir = os.environ['HSTDIR']
-dir = '/Users/aaw/data/'
+dir = os.environ['HSTDIR']
+#dir = '/Users/aaw/data/'
+
+Ba = [-1.019,-1.113,-1.026,-.990,-1.110,-.994,-.888]
+Bb = [1.937,2.065,1.954,1.883,2.018,1.804,1.758]
+B_coeff = [Ba,Bb]
+Va = [-.759,-.853,-.766,-.730,-.850,-.734,-.628]
+Vb = [1.537,1.665,1.554,1.483,1.618,1.404,1.358]
+V_coeff = [Va,Vb]
+Ja = [-.540,-.658,-.527,-.514,-.659,-.621,-.550]
+Jb = [.757,.907,.741,.704,.878,.794,.801]
+J_coeff = [Ja,Jb]
+coeff = [B_coeff, V_coeff, J_coeff]
+
 
 #setting up arrays with three elements, all zeros - placeholders
 wavelengths = ['F475W','F814W','F160W']
@@ -52,6 +80,7 @@ rSky[0]= (10.31036745,  10.5783909 ,  11.77636022,  11.28425007,11.90782971,  10
 rSky[1]= (10.64816775,  13.35756601,  10.54219567,  12.25892311,13.34688468,  10.75379768,  10.88078454,  10.26098238, 9.69418394,  10.592764, 10.57481831, 8.79336163)
 rSky[2]= (7.50788545,  7.58130161,  8.27527023,  9.03878993,  8.67763722, 7.62254201,  7.70920672,  6.74143006, 6.87375846,  7.46983987, 7.83102976,  8.10811507)
 
+solarLum = 3.846*10**33
 
 # specify the position of the science target and the size of the region around the science target to consider
 filters = np.array([475, 814, 1600]) #*u.nm
@@ -68,7 +97,7 @@ pixNoise = np.zeros([len(wavelengths), width, width])
 SNR = np.zeros([len(wavelengths), width, width])
 
 #Ldcm is the luminosity distance in cm, even though astropy thinks it is in Mpc. 
-Ldcm = cosmo.luminosity_distance(zs)*u.Mpc.to(u.cm) / u.Mpc
+lDcm = cosmo.luminosity_distance(zs)*u.Mpc.to(u.cm) / u.Mpc
 conv = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Om0=0.3)
 radToKpc = conv.arcsec_per_kpc_proper(zs)*0.05/u.arcsec*u.kpc
 totalphotons = 0
@@ -83,7 +112,8 @@ totalphotons = 0
     #    area[i] = math.pi*(math.pow(radii[i],2)-math.pow(radii[i-1],2))
 
 with PdfPages('sg_MONSTER.pdf') as pdf:
-    for w in range(0,1):
+    #for w in range(0,1):
+    for w in range(0,len(galaxies)):
         # for w in range(0,len(galaxies)):
         for i in range(0, len(wavelengths)):
             file = glob.glob(dir+galaxies[w]+'_final_'+wavelengths[i]+'*sci.fits')
@@ -108,10 +138,26 @@ with PdfPages('sg_MONSTER.pdf') as pdf:
 
 
         m = np.ma.masked_where(SNR<10,SNR)
+        n = np.ma.masked_where(SNR<10,fluxpix)
         for i in range(0,len(filters)):
-            fig = plt.figure()
-            plt.imshow(m[i])
-            plt.colorbar()
-            plt.title(galaxies[w]+' SNR at '+ str(wavelengths[i]))
-            pdf.savefig()
-            plt.close()
+            n[i]=n[i]*fnu[i]/exp[i]
+
+        colorUV = mag(n[0])-mag(n[1])
+        for i in range (0,len(filters)):
+            lum = luminosity(filters[i], n, lDcm[w])
+            for mod in range(5,6):
+                fig = plt.figure()
+                mlr = mLR(coeff[i][0][mod], coeff[i][1][mod], colorUV)
+                mass = lum*mlr
+                plt.imshow(m[i]*mass[i])
+                plt.colorbar()
+                plt.title(galaxies[w]+' Mass Profile in Solar Masses at ' + wavelengths[i], fontsize = 12)
+                pdf.savefig()
+                plt.close()
+                            
+        fig = plt.figure()
+        plt.imshow(colorUV)
+        plt.colorbar()
+        plt.title('ColorUV map and constrictions')
+        pdf.savefig()
+        plt.close()
