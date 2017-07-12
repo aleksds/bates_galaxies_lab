d1b1c8c2bcf04e888db96fef17a51c0e3ea46c8b
ashley warner <aaw@ashley-warners-computer.local>
2017-07-11 23:16:54 -0400

    fixed recursion, ready for bates computers. hasnot been run with all 12 galaxies

--- hst/sg_stealyobitch.py
+++ hst/sg_stealyobitch.py
@@ -10,10 +10,11 @@ from matplotlib.backends.backend_pdf import PdfPages
 from photutils import centroid_com, centroid_1dg, centroid_2dg
 
 #define a function to plot "postage stamp" images
-def plot_image(xcen,ycen, count):
-    print('fitting for ' + str(xcen) + ' and ' + str(ycen))
+def plot_image(posx,posy, count, prevstds):
+    stamp = data[int(round(posy-dy)):int(round(posy+dy)), int(round(posx-dx)):int(round(posx+dx))]
+
     count = count +1
-    print(str(count))
+
     std = np.std(stamp[stamp==stamp])
     x1, y1 = centroid_com(stamp)
     x2, y2 = centroid_1dg(stamp)
@@ -22,18 +23,18 @@ def plot_image(xcen,ycen, count):
     yavg = np.average([y1,y2,y3])
     xstd = np.std([x1,x2,x3])
     ystd = np.std([y1,y2,y3])
-    print(xstd, ystd)
+
     # RECURSION BITCH
-    if count < 100 and (xstd > 0.1 or ystd > 0.1):
-        
-        return plot_image(xavg, yavg, count)
+    if count < 100 and (xstd > 0.09 or ystd > 0.1) and (xstd <= prevstds[0] and ystd <= prevstds[1]):
+        print(count, posx-dx+xavg, posy-dy+yavg, xstd, ystd)
+        return plot_image(posx-dx+xavg, posy-dy+yavg, count, [xstd,ystd])
     else:
         return xavg, yavg, 1/(xstd**2), 1/(ystd**2), count
 
 
 # define the directory that contains the images
-#dir = os.environ['HSTDIR']
-dir = '/Users/aaw/data/'
+dir = os.environ['HSTDIR']
+#dir = '/Users/aaw/data/'
 
 # parameters for the initial guess at the galaxy centroid and the size
 Galaxies = [['J0826', 3629, 4154], ['J0901', 3934, 4137], ['J0905', 3387, 3503], ['J0944', 3477., 3405.], ['J1107', 3573, 3339.], ['J1219', 3803., 4170.], ['J1341', 3884, 4165], ['J1506', 4147., 3922.], ['J1558', 3787., 4186.], ['J1613', 4175., 3827.], ['J2116', 3567, 3436], ['J2140', 4067, 4054]]
@@ -52,8 +53,8 @@ ycen = np.zeros([len(Galaxies)])
 xstds = np.zeros([len(Galaxies)])
 ystds = np.zeros([len(Galaxies)])
 
-for j in range(0,1):
-#for j in range(0,len(Galaxies)):
+#for j in range(0,1):
+for j in range(0,len(Galaxies)):
     #xstds[j] = 1
     #ystds[j] = 1
         mxs = [0,0,0]
@@ -65,9 +66,7 @@ for j in range(0,1):
             hdu = fits.open(file[0])
             data, header = hdu[0].data, hdu[0].header
 
-            
-            stamp = data[round(Galaxies[j][2]-dy):round(Galaxies[j][2]+dy), round(Galaxies[j][1]-dx):round(Galaxies[j][1]+dx)]
-            mxs[i], mys[i], mstdx[i], mstdy[i], count = plot_image(Galaxies[j][1],Galaxies[j][2], 0)
+            mxs[i], mys[i], mstdx[i], mstdy[i], count = plot_image(Galaxies[j][1],Galaxies[j][2], 0, [1000, 1000])
             
         xcen[j] = np.average(mxs, weights = mstdx)
         ycen[j] = np.average(mys, weights = mstdy)
