289854a12f6f62bbbe04c871fba3938b940a441c
kdelva <kdelva@bates.edu>
2017-08-04 11:56:28 -0400

    Voigt components narrowed and efforts to limit flip profile between 2796 and 2803 made without success

--- hires/kd_covrfrac.py
+++ hires/kd_covrfrac.py
@@ -94,19 +94,33 @@ with PdfPages(filename) as pdf:
         vel_new = np.linspace(vel_median-1000, vel_median+1000, num=2001, endpoint=True)
 
 
+        # boom = len(vel_kms[0][test_2796])
+        # one = vel_kms[0][test_2796][round(boom*0.2)]
+        # two = vel_kms[0][test_2796][round(boom*0.4)]
+        # thr = vel_kms[0][test_2796][round(boom*0.6)]
+        # fou = vel_kms[0][test_2796][round(boom*0.8)]
+
         boom = len(vel_kms[0][test_2796])
-        one = vel_kms[0][test_2796][round(boom*0.2)]
-        two = vel_kms[0][test_2796][round(boom*0.4)]
-        thr = vel_kms[0][test_2796][round(boom*0.6)]
-        fou = vel_kms[0][test_2796][round(boom*0.8)]
+        one = vel_kms[0][test_2796][round(boom*0.1)]
+        two = vel_kms[0][test_2796][round(boom*0.2)]
+        thr = vel_kms[0][test_2796][round(boom*0.3)]
+        fou = vel_kms[0][test_2796][round(boom*0.4)]
+        fiv = vel_kms[0][test_2796][round(boom*0.5)]
+        six = vel_kms[0][test_2796][round(boom*0.6)]
+        sev = vel_kms[0][test_2796][round(boom*0.7)]
+        eig = vel_kms[0][test_2796][round(boom*0.8)]
+        nin = vel_kms[0][test_2796][round(boom*0.9)]
 
         flux_king = f(vel_new)
 
         xarr = vel_new
         yarr = flux_king - 1.
 
-        voi_init = Voigt1D(amplitude_L=-1.0, x_0=one, fwhm_L=two-one, fwhm_G=two-one)+Voigt1D(amplitude_L=-1.0, x_0=two, fwhm_L=thr-two, fwhm_G=thr-two)+Voigt1D(amplitude_L=-1.0, x_0=thr, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=fou, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=vel_median, fwhm_L=200, fwhm_G=200)
+        # voi_init = Voigt1D(amplitude_L=-1.0, x_0=one, fwhm_L=two-one, fwhm_G=two-one)+Voigt1D(amplitude_L=-1.0, x_0=two, fwhm_L=thr-two, fwhm_G=thr-two)+Voigt1D(amplitude_L=-1.0, x_0=thr, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=fou, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=vel_median, fwhm_L=200, fwhm_G=200)
+        
 
+        voi_init = Voigt1D(amplitude_L=-1.0, x_0=one, fwhm_L=two-one, fwhm_G=two-one)+Voigt1D(amplitude_L=-1.0, x_0=two, fwhm_L=thr-two, fwhm_G=thr-two)+Voigt1D(amplitude_L=-1.0, x_0=thr, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=fou, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=fiv, fwhm_L=fiv-fou, fwhm_G=fiv-fou)+Voigt1D(amplitude_L=-1.0, x_0=six, fwhm_L=six-fiv, fwhm_G=six-fiv)+Voigt1D(amplitude_L=-1.0, x_0=sev, fwhm_L=sev-six, fwhm_G=sev-six)+Voigt1D(amplitude_L=-1.0, x_0=eig, fwhm_L=eig-sev, fwhm_G=eig-sev)+Voigt1D(amplitude_L=-1.0, x_0=nin, fwhm_L=nin-eig, fwhm_G=nin-eig)+Voigt1D(amplitude_L=-1.0, x_0=vel_median, fwhm_L=200, fwhm_G=200)
+        
                    ## Write function that combines cover fraction code with Voigt profile code ??????????? ##
                    ##Correlation between amplitude and cover frac could be key##
                    
@@ -123,23 +137,34 @@ with PdfPages(filename) as pdf:
 
 
         boom_2803 = len(vel_kms[1][test_2803])
-        one_2803 = vel_kms[1][test_2803][round(boom_2803*0.2)]
-        two_2803 = vel_kms[1][test_2803][round(boom_2803*0.4)]
-        thr_2803 = vel_kms[1][test_2803][round(boom_2803*0.6)]
-        fou_2803 = vel_kms[1][test_2803][round(boom_2803*0.8)]
+        one_2803 = vel_kms[1][test_2803][round(boom_2803*0.1)]
+        two_2803 = vel_kms[1][test_2803][round(boom_2803*0.2)]
+        thr_2803 = vel_kms[1][test_2803][round(boom_2803*0.3)]
+        fou_2803 = vel_kms[1][test_2803][round(boom_2803*0.4)]
+        fiv_2803 = vel_kms[1][test_2803][round(boom_2803*0.5)]
+        six_2803 = vel_kms[1][test_2803][round(boom_2803*0.6)]
+        sev_2803 = vel_kms[1][test_2803][round(boom_2803*0.7)]
+        eig_2803 = vel_kms[1][test_2803][round(boom_2803*0.8)]
+        nin_2803 = vel_kms[1][test_2803][round(boom_2803*0.9)]
 
         flux_king_2803 = f(vel_new_2803)
 
         xarr_2803 = vel_new_2803
         yarr_2803 = flux_king_2803 - 1.
 
-        voi_init_2803 = Voigt1D(amplitude_L=-1.0, x_0=one_2803, fwhm_L=two_2803-one_2803, fwhm_G=two_2803-one_2803)+Voigt1D(amplitude_L=-1.0, x_0=two_2803, fwhm_L=thr_2803-two_2803, fwhm_G=thr_2803-two_2803)+Voigt1D(amplitude_L=-1.0, x_0=thr_2803, fwhm_L=fou_2803-thr_2803, fwhm_G=fou_2803-thr_2803)+Voigt1D(amplitude_L=-1.0, x_0=fou_2803, fwhm_L=fou_2803-thr_2803, fwhm_G=fou_2803-thr_2803)+Voigt1D(amplitude_L=-1.0, x_0=vel_median_2803, fwhm_L=200, fwhm_G=200)
+        # voi_init_2803 = Voigt1D(amplitude_L=-1.0, x_0=one_2803, fwhm_L=two_2803-one_2803, fwhm_G=two_2803-one_2803)+Voigt1D(amplitude_L=-1.0, x_0=two_2803, fwhm_L=thr_2803-two_2803, fwhm_G=thr_2803-two_2803)+Voigt1D(amplitude_L=-1.0, x_0=thr_2803, fwhm_L=fou_2803-thr_2803, fwhm_G=fou_2803-thr_2803)+Voigt1D(amplitude_L=-1.0, x_0=fou_2803, fwhm_L=fou_2803-thr_2803, fwhm_G=fou_2803-thr_2803)+Voigt1D(amplitude_L=-1.0, x_0=vel_median_2803, fwhm_L=200, fwhm_G=200)
+
+        voi_init_2803 = Voigt1D(amplitude_L=-1.0, x_0=one, fwhm_L=two-one, fwhm_G=two-one)+Voigt1D(amplitude_L=-1.0, x_0=two, fwhm_L=thr-two, fwhm_G=thr-two)+Voigt1D(amplitude_L=-1.0, x_0=thr, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=fou, fwhm_L=fou-thr, fwhm_G=fou-thr)+Voigt1D(amplitude_L=-1.0, x_0=fiv_2803, fwhm_L=fiv_2803-fou, fwhm_G=fiv_2803-fou)+Voigt1D(amplitude_L=-1.0, x_0=six_2803, fwhm_L=six_2803-fiv_2803, fwhm_G=six_2803-fiv_2803)+Voigt1D(amplitude_L=-1.0, x_0=sev_2803, fwhm_L=sev_2803-six_2803, fwhm_G=sev_2803-six_2803)+Voigt1D(amplitude_L=-1.0, x_0=eig_2803, fwhm_L=eig_2803-sev_2803, fwhm_G=eig_2803-sev_2803)+Voigt1D(amplitude_L=-1.0, x_0=nin_2803, fwhm_L=nin_2803-eig_2803, fwhm_G=nin_2803-eig_2803)+Voigt1D(amplitude_L=-1.0, x_0=vel_median_2803, fwhm_L=200, fwhm_G=200)
+        
         fitter_2803 = fitting.LevMarLSQFitter()
         voi_fit_2803 = fitter(voi_init_2803, xarr_2803, yarr_2803)
         
 
         ax.plot(xarr,voi_fit(xarr)+1, color='red')
         ax.plot(xarr_2803,voi_fit_2803(xarr_2803)+1, color='magenta')
+
+        # ax.plot(xarr[g2796],voi_fit(xarr)+1[g2796], color='red')
+        # ax.plot(xarr_2803[g2803],voi_fit_2803(xarr_2803)+1[g2803], color='magenta')
         plt.xlabel("Velocity(km/s)")
         plt.ylabel("C.N. Flux")
         ax.set_ylim (0,2)
