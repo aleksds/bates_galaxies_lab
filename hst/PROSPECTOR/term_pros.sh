ae238e17527c5b6cd14b88bb6fcd296a7c558cef
Sophia Gottlieb <sgottlie@a23368.bates.edu>
2017-11-13 16:45:43 -0500

    I think these work to run on 8 cores

---
+++ hst/PROSPECTOR/term_pros.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+python prospect_by_galaxy.py J0826_05 &
+python prospect_by_galaxy.py J0901_05 &
+python prospect_by_galaxy.py J0905_05 &
+python prospect_by_galaxy.py J0944_05 &
+python prospect_by_galaxy.py J1105_05 &
+python prospect_by_galaxy.py J1219_05 &
+python prospect_by_galaxy.py J1341_05 &
+python prospect_by_galaxy.py J1506_05 &
+
