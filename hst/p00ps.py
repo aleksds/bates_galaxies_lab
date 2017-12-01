535c218367d0eaead0c0efb9d7e72fd3f0048e6f
Sophia Gottlieb <sgottlie@a23368.bates.edu>
2017-12-01 15:05:52 -0500

    i dont understand why my computer hates me

--- hst/p00ps.py
+++ hst/p00ps.py
@@ -3,7 +3,7 @@
 # and graphs of the log of photons per pixel
 #
 # import relevant Python modules
-import os, sys
+import os
 import numpy as np
 from astropy.io import fits
 from photutils import CircularAperture
@@ -22,13 +22,6 @@ from astropy import units as u
 from astropy import constants as const
 from astropy.cosmology import FlatLambdaCDM
 from xlrd import open_workbook
-'''
-rawconv = sys.argv[1]
-coarsefine = sys.argv[2]
-'''
-
-rawconv = 0
-coarsefine = 0
 
 # define the directory that contains the images
 dir = os.environ['HSTDIR']
@@ -43,7 +36,6 @@ class Galaxy:
         self.radToKpc = conv.arcsec_per_kpc_proper(self.z)*0.05/u.arcsec*u.kpc
 # We grab our galaxy data and make a list of galaxy objects
 wbo = open_workbook('galaxydata.xlsx')
-#sheet = wbo.sheets()[0]
 for sheet in wbo.sheets():
     numr = sheet.nrows
     galaxies = []
@@ -67,6 +59,21 @@ pixr = int((width-1)/2)
 
 # specify the position of the science target and the size of the region around the science target to consider
 
+'''
+galaxies = ['J0826', 'J0901', 'J0905', 'J0944', 'J1107', 'J1219', 'J1341', 'J1506', 'J1558', 'J1613', 'J2116', 'J2140']
+xcen = [3628, 3933, 3386.5, 3477.5, 3573, 3802, 3886, 4149, 3787, 4174, 3565, 4067]
+ycen = [4153, 4136, 3503.2, 3404.3, 3339, 4169, 4164, 3921, 4187, 3826, 3434, 4054]
+
+
+
+zs = [0.603, 0.459, 0.712, 0.514, 0.467, 0.451, 0.658, 0.608, 0.402, 0.449, 0.728, 0.752]
+
+conv = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Om0=0.3)
+radToKpc = conv.arcsec_per_kpc_proper(zs)*0.05/u.arcsec*u.kpc
+
+#Ldcm is the luminosity distance in cm, even though astropy thinks it is in Mpc. 
+Ldcm = cosmo.luminosity_distance(zs)*u.Mpc.to(u.cm) / u.Mpc
+'''
 totalphotons = 0
 fluxvalues = [[0 for x in range(len(wavelengths))] for y in range(len(galaxies))]
 # define the radii to be used for aperture photometry
@@ -86,12 +93,16 @@ for i in range(0, len(area)):
 
 # Now, we loop through all galaxies
 res = ['fine','coarse']
-proc = ['convolved_image', 'final']
+type = ['convolved_image', 'final']
 with PdfPages('sg_FLUX.pdf') as pdf: 
+
+#for w in range (2,3):
+    # Mostly just for our own benefit, we print the galaxy's name so we know what 
+    # works and what doesn't when the code breaks.
     
     for w in galaxies:
         
-        print(w)
+        print(w.name)
         fig = plt.figure()
         collection = ['F475W','F814W','F160W']
     
@@ -101,7 +112,7 @@ with PdfPages('sg_FLUX.pdf') as pdf:
         for i in range (0, len(collection)):
             
             # read in the images
-            file = glob.glob(dir+w.name+'*/'+res[coarsefine]+'/'+str(filters[i])+'/'+proc[rawconv]+'_'+str(filters[i])+'*sci.fits')
+            file = glob.glob(dir+w.name+'*/'+res[1]+'/'+str(filters[i])+'/'+type[1]+'_'+str(filters[i])+'*sci.fits')
             #file = glob.glob(dir+galaxies[w]+'_final_'+collection[i]+'*sci.fits')
             hdu = fits.open(file[0])
             data[i], header[i] = hdu[0].data, hdu[0].header
@@ -146,26 +157,28 @@ with PdfPages('sg_FLUX.pdf') as pdf:
                     subflux[i,j] = flux[i,j]-flux[i,j-1]
 
             fluxvalues[galaxies.index(w)]=subflux
-print('WHY DONT YOU LOVE ME')
 
-photfolder = 'PROSPECTOR/'+res[rawconv]+'_'+proc[coarsefine]+'/'
-if not os.path.exists(photfolder):
-    os.makedirs(photfolder)
-    
+# Now that fluxvalues is full, we have to start making a new table.....
+# the table has 8 columns: ID, f475, iv475, f814, iv814, f1600, iv1600, and z
+# ID is GalaxyName_Aperature and all data is in nanomaggys
+'''
+# we create or open our txt file
+f = open("sg_fluxtable_nm.txt","w+")
+
+# we write our column titles - unsure if these need to stay, just thought it would be nice.
+f.write('ID\t\tf_475\t\tivar_475\t\tf_814\t\tivar_814\t\tf_1600\t\tivar_1600\t\tz\n')
+
 # we shift through the data by galaxy and then by aperture.
 for w in range(0,len(galaxies)):
-    gal = galaxies[w].name[:5]
     # we create or open our txt file
-    f = open(photfolder+ gal+'.txt',"w+")
-    print(photfolder+ gal+'.txt')
+    f = open(galaxies[w].name+"_nm.txt","w+")
 
     # we write our column titles - unsure if these need to stay, just thought it would be nice.
     f.write('ID\t\tf_475\t\tivar_475\t\tf_814\t\tivar_814\t\tf_1600\t\tivar_1600\t\tz\n')
     
     for i in range(0,len(radii)):
         # Building the ID name using if/else for those with single digits.
-        
-        ID = gal+'_'
+        ID = galaxies[w].name+'_'
         if i < 10:
             ID = ID + '0' + str(i)
         else:
@@ -181,5 +194,5 @@ for w in range(0,len(galaxies)):
         # to conclude the line, we include the z value for the galaxy before calling for a new line (\n).
         f.write(str(galaxies[w].z)+'\n')
 
-    f.close()
-print('YOURE NOT MY REAL MOM')
+f.close()
+'''
