6a8e88cef966dabc6c5bcaa5f9801bf6d8a655ee
Sophia Gottlieb <sgottlie@a23368.bates.edu>
2017-12-13 11:25:13 -0500

    still needs no axis, composite image

--- hst/sg_posterwindow.py
+++ hst/sg_posterwindow.py
@@ -12,8 +12,14 @@ from matplotlib import cm
 import matplotlib.colors as colors
 from matplotlib.backends.backend_pdf import PdfPages
 import math
+from astropy.cosmology import WMAP9 as cosmo
+from astropy import units as u
+from astropy import constants as const
+from astropy.cosmology import FlatLambdaCDM 
 import matplotlib.lines as mlines
 from matplotlib.legend_handler import HandlerLine2D
+
+conv = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Om0=0.3)
 
 from xlrd import open_workbook
 
@@ -51,11 +57,32 @@ dir = os.environ['HSTDIR']
 # define a function to plot "postage stamp" images
 def plot_image():
     std = np.std(stamp[stamp==stamp])
-    plt.imshow(stamp, interpolation='nearest', origin = 'lower', vmin = -1.*std, vmax = 3.*std, cmap='bone')
+    plt.imshow(stamp, interpolation='nearest', origin = 'lower', vmin = -1.*std, vmax = 3.*std, cmap='Greys')
     plt.tick_params(axis='both', which='major', labelsize=8)
+    
+filters = ['F475W','F814W','F160W']
+res = ['fine','coarse']
+type = ['convolved_image', 'final']
 
-
-with PdfPAges('poster.pdf') as pdf:
-    for w in galaxies:
+with PdfPages('poster.pdf') as pdf:
+    for g, gal in enumerate(galaxies):
         fig = plt.figure()
-        file = glob.glob(dir+
+        plt.suptitle(gal.name)
+        for f, fil in enumerate(filters):
+            file = glob.glob(dir+gal.name+'*/'+res[1]+'/'+fil+'/'+type[1]+'_'+fil+'*sci.fits')
+
+            hdu = fits.open(file[0])
+            data, header = hdu[0].data, hdu[0].header
+            width = 100
+            dy = width
+            dx = width
+
+            stamp = data[round(gal.y-dy):round(gal.y+dy), round(gal.x-dx):round(gal.x+dx)]
+            #ax = fig.add_subplot(len(galaxies),len(filters), 1+g*4+f)
+            ax = fig.add_subplot(1,len(filters)+1, 1+f)
+            plot_image()
+            plt.title(fil)
+            
+        pdf.savefig()
+        plt.close()
+                             
