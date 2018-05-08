0bb8d7255a8eed086c611ce16e3899f4a787060b
Charles Lipscomb <clipscom@bates.edu>
2018-05-08 14:02:23 -0400

    makes tables with flux values from mag values from galfit runs in subdirectories (also in tables are inverse variances with5% error assumed)

--- hst/cl_stellarmassallgal.py
+++ hst/cl_stellarmassallgal.py
@@ -2,16 +2,32 @@
 #10^(magnitude/(-2.5))*10^(9) = flux in nanomaggies
 import numpy as np
 from astropy.table import Table
-#magnitudes
-vmag = [19.494,19.468,19.54,19.819,19.394,19.389,19.048,19.112,19.498,19.719,19.905,20.329]
-imag = [18.647,18.496,18.984,19.038,18.569,18.155,18.544,18.642,18.143,18.5,19.104,18.934]
+import sys
+
+#the code below allows one to provide an argument when using ipython so one can put in the desired folder (which is named based on the date and time)
+date_time = sys.argv[1]
+print(date_time)
+
+galaxies = ['J0826', 'J0901', 'J0905', 'J0944', 'J1107', 'J1219', 'J1341', 'J1506', 'J1558', 'J1613', 'J2116', 'J2140']
+filters = ['F475W','F814W']
+
+
+
+#SERSIC-INDEPENDENT
+mags = np.zeros([12,2])
+for w in range(0,12):
+    for i in range(0,2):
+        file = 'autogalfit/'+date_time+'_sersic_independent/'+galaxies[w]+'_'+filters[i]+'_coarse.galfit.01.band'
+        with open(file) as f:
+            content = f.readlines()
+        mags[w][i] = np.float(content[47][4:10])
 
 #flux in nanomaggies
 vflux=np.zeros(12)
 iflux=np.zeros(12)
 for w in range(0,12):
-    vflux[w] = (10**(vmag[w]/(-2.5))*10**9)
-    iflux[w] = (10**(imag[w]/(-2.5))*10**9)
+    vflux[w] = (10**(mags[w][0]/(-2.5))*10**9)
+    iflux[w] = (10**(mags[w][1]/(-2.5))*10**9)
 
 #inverse variances
 vivar=np.zeros(12)
@@ -20,26 +36,26 @@ for i in range(0,12):
     vivar[i] = 1/(vflux[i]*0.05)**2
     iivar[i] = 1/(iflux[i]*0.05)**2
 
-#fsfflux = [vflux]
-#fsfivar = [vivar]
-#efflux = [iflux]
-#efivar = [iivar]
-table = Table([vflux, vivar, iflux, iivar], names=('475 flux', '475 ivar', '814 flux', '814 ivar'))
+table = Table([vflux, vivar, iflux, iivar], names=('sersic-indy 475 flux', '475 ivar', '814 flux', '814 ivar'))
 print(table)
 
 
 
-
-#now for psf fits
-vmag = [19.686,19.627,19.647,19.929,19.699,19.995,19.192,19.183,19.949,20.598,20.262,20.431]
-imag = [19.253,19.138,19.19,19.263,19.427,19.137,18.96,19.048,19.661,19.63,19.601,19.63]
+#SERSIC-SIMULTANEOUS
+mags = np.zeros([12,2])
+for w in range(0,12):
+    file = 'autogalfit/'+date_time+'_sersic_simultaneous/'+galaxies[w]+'_F814W_F475W_sersic_output.galfit.01.band'
+    with open(file) as f:
+        content = f.readlines()
+    mags[w][0] = np.float(content[47][12:17])
+    mags[w][1] = np.float(content[47][4:10])
 
 #flux in nanomaggies
 vflux=np.zeros(12)
 iflux=np.zeros(12)
 for w in range(0,12):
-    vflux[w] = (10**(vmag[w]/(-2.5))*10**9)
-    iflux[w] = (10**(imag[w]/(-2.5))*10**9)
+    vflux[w] = (10**(mags[w][0]/(-2.5))*10**9)
+    iflux[w] = (10**(mags[w][1]/(-2.5))*10**9)
 
 #inverse variances
 vivar=np.zeros(12)
@@ -48,9 +64,61 @@ for i in range(0,12):
     vivar[i] = 1/(vflux[i]*0.05)**2
     iivar[i] = 1/(iflux[i]*0.05)**2
 
-#fsfflux = [vflux]
-#fsfivar = [vivar]
-#efflux = [iflux]
-#efivar = [iivar]
-table = Table([vflux, vivar, iflux, iivar], names=('475 flux', '475 ivar', '814 flux', '814 ivar'))
+table = Table([vflux, vivar, iflux, iivar], names=('sersic-simult 475 flux', '475 ivar', '814 flux', '814 ivar'))
+print(table)
+
+
+
+#PSF-INDEPENDENT
+mags = np.zeros([12,2])
+for w in range(0,12):
+    for i in range(0,2):
+        file = 'autogalfit/'+date_time+'_psf_independent/'+galaxies[w]+'_'+filters[i]+'_coarse.galfit.01.band'
+        with open(file) as f:
+            content = f.readlines()
+        mags[w][i] = np.float(content[47][4:10])
+
+#flux in nanomaggies
+vflux=np.zeros(12)
+iflux=np.zeros(12)
+for w in range(0,12):
+    vflux[w] = (10**(mags[w][0]/(-2.5))*10**9)
+    iflux[w] = (10**(mags[w][1]/(-2.5))*10**9)
+
+#inverse variances
+vivar=np.zeros(12)
+iivar=np.zeros(12)
+for i in range(0,12):
+    vivar[i] = 1/(vflux[i]*0.05)**2
+    iivar[i] = 1/(iflux[i]*0.05)**2
+
+table = Table([vflux, vivar, iflux, iivar], names=('psf-indy 475 flux', '475 ivar', '814 flux', '814 ivar'))
+print(table)
+
+
+
+#PSF-SIMULTANEOUS
+mags = np.zeros([12,2])
+for w in range(0,12):
+    file = 'autogalfit/'+date_time+'_psf_simultaneous/'+galaxies[w]+'_F814W_F475W_psf_output.galfit.01.band'
+    with open(file) as f:
+        content = f.readlines()
+    mags[w][0] = np.float(content[47][12:17])
+    mags[w][1] = np.float(content[47][4:10])
+
+#flux in nanomaggies
+vflux=np.zeros(12)
+iflux=np.zeros(12)
+for w in range(0,12):
+    vflux[w] = (10**(mags[w][0]/(-2.5))*10**9)
+    iflux[w] = (10**(mags[w][1]/(-2.5))*10**9)
+
+#inverse variances
+vivar=np.zeros(12)
+iivar=np.zeros(12)
+for i in range(0,12):
+    vivar[i] = 1/(vflux[i]*0.05)**2
+    iivar[i] = 1/(iflux[i]*0.05)**2
+
+table = Table([vflux, vivar, iflux, iivar], names=('psf-simult 475 flux', '475 ivar', '814 flux', '814 ivar'))
 print(table)
