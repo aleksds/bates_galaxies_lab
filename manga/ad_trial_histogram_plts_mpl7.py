04a16ebdaa0605653cbf34bb2ab3997b4dcd5aad
Aleks Diamond-Stanic <aleks.diamond.stanic@gmail.com>
2018-06-15 23:54:04 -0600

    version that looks at concentration and b/a histograms for all manga galaxies and for edge-on late types

---
+++ manga/ad_trial_histogram_plts_mpl7.py
@@ -0,0 +1,129 @@
+import os
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+ 
+drp = fits.open('drpall-v2_4_3.fits')
+drpdata = drp[1].data 
+
+
+ba = drpdata.field('nsa_sersic_ba')
+stellar_mass = drpdata.field('nsa_sersic_mass')
+absmag = drpdata.field('nsa_elpetro_absmag')
+gr= absmag[:,3] - absmag[:,4]
+filename = 'histograms.pdf'
+
+# read in information from NSA catalog
+nsa = fits.open('1-nsa_v1_0_1.fits')
+nsa_data = nsa[1].data
+
+# check on a galaxy of interest
+plate = 7977
+galaxy = 12704
+match = np.where((drpdata.field('plate') == plate) & (drpdata.field('ifudsgn') == str(galaxy)))
+mangaid = drpdata[match].field('mangaid')[0]
+nsaid = drpdata[match].field('nsa_nsaid')[0]
+
+yo = np.where(nsa_data.field('NSAID') == nsaid)
+name = nsa_data[yo[0][0]].field('IAUNAME')
+print(name)
+blah = drpdata[match].field('nsa_iauname')[0]
+print(blah)
+
+# match the two catalogs on the basis of right ascension and declination
+manga_ra = drpdata.field('objra')
+manga_dec = drpdata.field('objdec')
+nsa_ra = nsa_data.field('RA')
+nsa_dec = nsa_data.field('DEC')
+c = SkyCoord(ra=manga_ra*u.degree, dec=manga_dec*u.degree)
+catalog = SkyCoord(ra=nsa_ra*u.degree, dec=nsa_dec*u.degree)
+idx, d2d, d3d = c.match_to_catalog_sky(catalog)  
+
+# define th50, th90, and concentration arrays
+th50 = nsa_data.field('ELPETRO_TH50_R')
+th90 = nsa_data.field('ELPETRO_TH90_R')
+
+th50_manga = th50[idx]
+th90_manga = th90[idx]
+c_manga = th90_manga / th50_manga
+
+# get just the MaNGA galaxies
+blah = drpdata.field('srvymode') == 'MaNGA dither'
+print(len(c_manga[blah]))
+
+# update the concentration and ba arrays
+c_manga_gal = c_manga[blah]
+ba_gal = ba[blah]
+
+# find late-type manga galaxies
+late = c_manga_gal < 2.6
+
+# update concentration and ba arrays
+ba_gal_late = ba_gal[late]
+c_manga_gal_late = c_manga_gal[late]
+
+# find edge-one late-type galaxies
+edge = ba_gal_late < 0.3
+
+# update concentration and ba arrays
+c_manga_gal_late_edge = c_manga_gal_late[edge]
+ba_gal_late_edge = ba_gal_late[edge]
+
+print(len(c_manga_gal_late_edge))
+print(len(ba_gal_late_edge))
+
+with PdfPages(filename) as pdf:
+
+    #plot 1: b/a ratio
+    fig = plt.figure()
+    plt.xlim(xmin=0.0, xmax = 1.05)
+    plt.hist(ba_gal, color='teal', bins=np.arange(0.05,1.02,.02))
+    plt.hist(ba_gal_late_edge, color='red', bins=np.arange(0.05,1.02,.02)) 
+    plt.xlabel('b/a ratio')
+    plt.ylabel('Number of Galaxies')
+    plt.title('Distribution of b/a Values in MaNGA sample')
+
+    pdf.savefig()
+    plt.close
+
+#    #plot 2: stellar mass
+#    fig = plt.figure()
+#    print(np.shape(drpdata))
+#    plt.hist(np.log10(stellar_mass[wanted]),color='teal', bins=np.arange(8.0,12.0,.2))
+#    plt.xlim(8,12)
+#    plt.xlabel('Stellar Mass (logM☉)')
+#    plt.ylabel('Number of Galaxies')
+#    plt.title('Distribution of Stellar Mass in MaNGA sample')
+#
+#    pdf.savefig()
+#    plt.close
+
+#    #plot 3: g-r
+#    fig = plt.figure()
+#    print(np.shape(drpdata))
+#    plt.hist(gr[wanted], bins=np.arange(0.0,1.0,0.05), color='teal')
+#    plt.xlabel('g-r')
+#    plt.ylabel('Number of Galaxies')
+#    plt.title('Distribution of g-r in MaNGA sample')
+#
+#    pdf.savefig()
+#    plt.close
+
+    #concentration
+    fig = plt.figure()
+    plt.xlim(xmin=0, xmax = 4.5)
+    plt.hist(c_manga_gal, bins=np.arange(.25,4.1,0.1), color='teal')
+    plt.hist(c_manga_gal_late_edge, bins=np.arange(.25,4.1,0.1), color='red')
+    plt.xlabel('Concentration')
+    plt.ylabel('Number of Galaxies')
+    plt.title('Distribution of Concentration in MaNGA sample')
+
+    pdf.savefig()
+    plt.close
+    
+    
+os.system("open %s &" % filename)
+    
