9c33d6acf47c5ed7f67409aa47f4a52fd9d2f9d2
rminsley <rminsley@bates.edu>
2018-07-12 16:38:12 -0400

    includes vdis/rot

--- manga/asymmetry_parameter.py
+++ manga/asymmetry_parameter.py
@@ -79,6 +79,7 @@ th50_manga = th50[idx]
 th90_manga = th90[idx]
 c_manga = th90_manga / th50_manga
 
+
 # get just the MaNGA galaxies
 blah = drpdata.field('srvymode') == 'MaNGA dither'
 # update the concentration and ba arrays
@@ -90,154 +91,219 @@ ba_gal_late = ba_gal[late]
 edge = abs(ba_gal_late) < 0.3
 ba_gal_late_edge = ba_gal_late[edge]
 good_plates = np.sort(plateifu[blah][late][edge])
+elp_50 = th50_manga[blah][late][edge]
 
 total = len(good_plates)
 poop = np.zeros(total)
+poo_p = np.zeros(total)
+poo_n = np.zeros(total)
 
-for i in range(0,total):
-    plate, ifu = good_plates[i].split('-')
-    name = mpl7_dir + 'HYB10-GAU-MILESHC/' + str(plate) + '/' + str(ifu) + '/manga-' + str(plate) + '-' + str(ifu) + '-MAPS-HYB10-GAU-MILESHC.fits.gz'
-    if os.path.isfile(name):
-        match = np.where((drpdata.field('plate') == int(plate)) & (drpdata.field('ifudsgn') == str(ifu)))
-        hdu_dap = fits.open(name)
-        print(name)
+filename = 'xi_plotg.pdf'
+with PdfPages(filename) as pdf:
+    for i in range(0,total):
+        plate, ifu = good_plates[i].split('-')
+        name = mpl7_dir + 'HYB10-GAU-MILESHC/' + str(plate) + '/' + str(ifu) + '/manga-' + str(plate) + '-' + str(ifu) + '-MAPS-HYB10-GAU-MILESHC.fits.gz'
+        if os.path.isfile(name):
+            match = np.where((drpdata.field('plate') == int(plate)) & (drpdata.field('ifudsgn') == str(ifu)))
+            hdu_dap = fits.open(name)
+            print(name)
 
-        # hdu_dap.info()
-        emlc = channel_dictionary(hdu_dap, 'EMLINE_SFLUX')
-        dap_ha_sflux = hdu_dap['EMLINE_SFLUX'].data[emlc['Ha-6564'], :, :]
-        dap_ha_sivar = hdu_dap['EMLINE_SFLUX_IVAR'].data[emlc['Ha-6564'], :, :]
+            # hdu_dap.info()
+            emlc = channel_dictionary(hdu_dap, 'EMLINE_SFLUX')
+            dap_ha_sflux = hdu_dap['EMLINE_SFLUX'].data[emlc['Ha-6564'], :, :]
+            dap_ha_sivar = hdu_dap['EMLINE_SFLUX_IVAR'].data[emlc['Ha-6564'], :, :]
 
-        # read in the position angle and axis from from the NSA
-        theta = drpdata.field('nsa_sersic_phi')[match][0]
-        ba = drpdata.field('nsa_sersic_ba')[match][0]
-        inc = np.arccos(ba)
+            # read in the position angle and axis from from the NSA
+            theta = drpdata.field('nsa_sersic_phi')[match][0]
+            ba = drpdata.field('nsa_sersic_ba')[match][0]
+            inc = np.arccos(ba)
 
 
-        # create arrays that correspond to x and y coordinates for each spaxel
-        size = len(hdu_dap['EMLINE_GFLUX'].data[0, :])
-        xpos = np.zeros([size, size])
-        ypos = np.zeros([size, size])
-        for j in range(0, size):
-            for k in range(0, size):
-                xpos[j, k] = k
-                ypos[j, k] = j
+            # create arrays that correspond to x and y coordinates for each spaxel
+            size = len(hdu_dap['EMLINE_GFLUX'].data[0, :])
+            xpos = np.zeros([size, size])
+            ypos = np.zeros([size, size])
+            for j in range(0, size):
+                for k in range(0, size):
+                    xpos[j, k] = k
+                    ypos[j, k] = j
 
-        # redefine x=0 and y=0 to be in the center of the field
-        xprime = xpos - np.median(xpos)
-        yprime = ypos - np.median(ypos)
+            # redefine x=0 and y=0 to be in the center of the field
+            xprime = xpos - np.median(xpos)
+            yprime = ypos - np.median(ypos)
 
-        # compute the on-sky x and y coordiates defined by the major axis
-        trad = np.radians(theta - 90)
-        xproj = xprime * np.cos(trad) + yprime * np.sin(trad)
-        yproj = xprime * np.sin(trad) * (-1.) + yprime * np.cos(trad)
-        zproj = yproj / np.sin(inc) * np.cos(inc)
+            # compute the on-sky x and y coordiates defined by the major axis
+            trad = np.radians(theta - 90)
+            xproj = xprime * np.cos(trad) + yprime * np.sin(trad)
+            yproj = xprime * np.sin(trad) * (-1.) + yprime * np.cos(trad)
+            zproj = yproj / np.sin(inc) * np.cos(inc)
 
-        # calculate the radius of each pixel in the plane of the disk [units: pixels]
-        radpix = np.sqrt(xproj ** 2 + (yproj / ba) ** 2)
+            # calculate the radius of each pixel in the plane of the disk [units: pixels]
+            radpix = np.sqrt(xproj ** 2 + (yproj / ba) ** 2)
 
-        # figure out the conversion between pixel size and kpc
-        z = drpdata.field('nsa_z')[match][0]
-        radkpc = radpix / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
+            # figure out the conversion between pixel size and kpc
+            z = drpdata.field('nsa_z')[match][0]
+            radkpc = radpix / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
 
-        # compute values along the x and y axis of the disk and the z axis above the disk
-        xproj_kpc = xproj / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
-        yproj_kpc = yproj / ba / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
-        zproj_kpc = zproj / ba / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
-        cen = abs(xproj_kpc) < (1. * u.kpc)
+            # compute values along the x and y axis of the disk and the z axis above the disk
+            xproj_kpc = xproj / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
+            yproj_kpc = yproj / ba / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
+            zproj_kpc = zproj / ba / cosmo.arcsec_per_kpc_proper(z) * (0.5 * u.arcsec)
+            cen = abs(xproj_kpc) < (1. * u.kpc)
 
-        radkpc_map = np.zeros([size, size])
-        xproj_kpc_map = np.zeros([size, size])
-        yproj_kpc_map = np.zeros([size, size])
-        zproj_kpc_map = np.zeros([size, size])
+            radkpc_map = np.zeros([size, size])
+            xproj_kpc_map = np.zeros([size, size])
+            yproj_kpc_map = np.zeros([size, size])
+            zproj_kpc_map = np.zeros([size, size])
 
-        for j in range(0, size):
-            for k in range(0, size):
-                if (dap_ha_sivar[j, k] > 0.):
-                    radkpc_map[j, k] = radkpc[j, k] / u.kpc
-                    yproj_kpc_map[j, k] = yproj_kpc[j, k] / u.kpc
-                    zproj_kpc_map[j, k] = zproj_kpc[j, k] / u.kpc
-                else:
-                    radkpc_map[j, k] = radkpc[j, k] / u.kpc * (-1.)
+            for j in range(0, size):
+                for k in range(0, size):
+                    if (dap_ha_sivar[j, k] > 0.):
+                        radkpc_map[j, k] = radkpc[j, k] / u.kpc
+                        yproj_kpc_map[j, k] = yproj_kpc[j, k] / u.kpc
+                        zproj_kpc_map[j, k] = zproj_kpc[j, k] / u.kpc
+                    else:
+                        radkpc_map[j, k] = radkpc[j, k] / u.kpc * (-1.)
 
-        # hdu_dap['EMLINE_GFLUX'].header
-        # C01     = 'OII-3727'           / Data in channel 1
-        # C02     = 'OII-3729'           / Data in channel 2
-        # C03     = 'Hthe-3798'          / Data in channel 3
-        # C04     = 'Heta-3836'          / Data in channel 4
-        # C05     = 'NeIII-3869'         / Data in channel 5
-        # C06     = 'Hzet-3890'          / Data in channel 6
-        # C07     = 'NeIII-3968'         / Data in channel 7
-        # C08     = 'Heps-3971'          / Data in channel 8
-        # C09     = 'Hdel-4102'          / Data in channel 9
-        # C10     = 'Hgam-4341'          / Data in channel 10
-        # C11     = 'HeII-4687'          / Data in channel 11
-        # C12     = 'Hb-4862 '           / Data in channel 12
-        # C13     = 'OIII-4960'          / Data in channel 13
-        # C14     = 'OIII-5008'          / Data in channel 14
-        # C15     = 'HeI-5877'           / Data in channel 15
-        # C16     = 'OI-6302 '           / Data in channel 16
-        # C17     = 'OI-6365 '           / Data in channel 17
-        # C18     = 'NII-6549'           / Data in channel 18
-        # C19     = 'Ha-6564 '           / Data in channel 19
-        # C20     = 'NII-6585'           / Data in channel 20
-        # C21     = 'SII-6718'           / Data in channel 21
-        # C22     = 'SII-6732'           / Data in channel 22
+            # hdu_dap['EMLINE_GFLUX'].header
+            # C01     = 'OII-3727'           / Data in channel 1
+            # C02     = 'OII-3729'           / Data in channel 2
+            # C03     = 'Hthe-3798'          / Data in channel 3
+            # C04     = 'Heta-3836'          / Data in channel 4
+            # C05     = 'NeIII-3869'         / Data in channel 5
+            # C06     = 'Hzet-3890'          / Data in channel 6
+            # C07     = 'NeIII-3968'         / Data in channel 7
+            # C08     = 'Heps-3971'          / Data in channel 8
+            # C09     = 'Hdel-4102'          / Data in channel 9
+            # C10     = 'Hgam-4341'          / Data in channel 10
+            # C11     = 'HeII-4687'          / Data in channel 11
+            # C12     = 'Hb-4862 '           / Data in channel 12
+            # C13     = 'OIII-4960'          / Data in channel 13
+            # C14     = 'OIII-5008'          / Data in channel 14
+            # C15     = 'HeI-5877'           / Data in channel 15
+            # C16     = 'OI-6302 '           / Data in channel 16
+            # C17     = 'OI-6365 '           / Data in channel 17
+            # C18     = 'NII-6549'           / Data in channel 18
+            # C19     = 'Ha-6564 '           / Data in channel 19
+            # C20     = 'NII-6585'           / Data in channel 20
+            # C21     = 'SII-6718'           / Data in channel 21
+            # C22     = 'SII-6732'           / Data in channel 22
 
-        for j in range(0, 1):
-            # DAP: emission-line quantities
-            dap_vel = hdu_dap['EMLINE_GVEL'].data[j, :, :]
-            dap_vel_ivar = hdu_dap['EMLINE_GVEL_IVAR'].data[j, :, :]
-            dap_vel_err = np.sqrt(1. / dap_vel_ivar)
-            dap_sig = hdu_dap['EMLINE_GSIGMA'].data[j, :, :]
-            dap_sig_ivar = hdu_dap['EMLINE_GSIGMA'].data[j, :, :]
-            dap_sig_err = np.sqrt(1. / dap_sig_ivar)
-            dap_flux = hdu_dap['EMLINE_GFLUX'].data[j, :, :]
-            dap_mask = hdu_dap['EMLINE_GFLUX_MASK'].data[j, :, :]
-            dap_ivar = hdu_dap['EMLINE_GFLUX_IVAR'].data[j, :, :]
-            dap_err = np.sqrt(1. / dap_ivar)
-            dap_sflux = hdu_dap['EMLINE_SFLUX'].data[j, :, :]
-            dap_smask = hdu_dap['EMLINE_SFLUX_MASK'].data[j, :, :]
-            dap_sivar = hdu_dap['EMLINE_SFLUX_IVAR'].data[j, :, :]
-            dap_serr = np.sqrt(1. / dap_sivar)
+            for j in range(0, 1):
+                # DAP: emission-line quantities
+                dap_vel = hdu_dap['EMLINE_GVEL'].data[j, :, :]
+                dap_vel_ivar = hdu_dap['EMLINE_GVEL_IVAR'].data[j, :, :]
+                dap_vel_err = np.sqrt(1. / dap_vel_ivar)
+                dap_sig = hdu_dap['EMLINE_GSIGMA'].data[j, :, :]
+                dap_sig_ivar = hdu_dap['EMLINE_GSIGMA'].data[j, :, :]
+                dap_sig_err = np.sqrt(1. / dap_sig_ivar)
+                dap_flux = hdu_dap['EMLINE_GFLUX'].data[j, :, :]
+                dap_mask = hdu_dap['EMLINE_GFLUX_MASK'].data[j, :, :]
+                dap_ivar = hdu_dap['EMLINE_GFLUX_IVAR'].data[j, :, :]
+                dap_err = np.sqrt(1. / dap_ivar)
+                dap_sflux = hdu_dap['EMLINE_SFLUX'].data[j, :, :]
+                dap_smask = hdu_dap['EMLINE_SFLUX_MASK'].data[j, :, :]
+                dap_sivar = hdu_dap['EMLINE_SFLUX_IVAR'].data[j, :, :]
 
-            # getting rid of bad spaxels
-            # s_n = dap_sflux / dap_serr
-            # bad = (s_n < 2.5)
-            # s_n[bad] = 0
-            # dap_vel[bad] = -2000
-            # for b in range(0, len(dap_vel[i])):
-            #     if dap_vel[b].all() < -1999:
-            #         dap_vel.remove(dap_vel[b].all())
-            #         dap_vel_ivar.remove(dap_vel[b].all())
+                # calculating noise variables
+                dap_serr = np.sqrt(1. / dap_sivar)
+                s_n_ha = dap_ha_sflux * np.sqrt(dap_ha_sivar)                           # signal to noise h alpha
+                snb = s_n_ha < 5
 
-            # creating vel_flip map
-            vel_flip = np.zeros([size, size])
-            ivar_flip = np.zeros([size, size])
+                # creating maps flipped across the major axis
+                vel_flip = np.zeros([size, size])
+                ivar_flip = np.zeros([size, size])
+                for m in range(0, size):
+                    for n in range(0, size):
+                        dist = np.sqrt((xproj_kpc - xproj_kpc[m, n]) ** 2 + (yproj_kpc + yproj_kpc[m, n]) ** 2)
+                        test = (dist == np.min(dist))
+                        vel_flip[m, n] = dap_vel[test]
+                        ivar_flip[m, n] = dap_vel_ivar[test]
 
-            for m in range(0, size):
-                for n in range(0, size):
-                    dist = np.sqrt((xproj_kpc - xproj_kpc[m, n]) ** 2 + (yproj_kpc + yproj_kpc[m, n]) ** 2)
-                    test = (dist == np.min(dist))
-                    vel_flip[m, n] = dap_vel[test]
-                    ivar_flip[m, n] = dap_vel_ivar[test]
+                # calculating asymmetry parameter including spaxels with s_n(h alpha) < 5
+                delta_vel = dap_vel-vel_flip
+                denom = np.sqrt((1/dap_ivar)+(1/ivar_flip))
+                frac = delta_vel/denom                                                  # asymmetry parameter before taking the std
+                re_arcsec = drpdata.field('nsa_elpetro_th50_r')[match] + np.array([1])
+                re_kpc = re_arcsec * u.arcsec / cosmo.arcsec_per_kpc_proper(z)
+                re_large = np.ravel(radkpc) > re_kpc
+                re_large_positive = np.ravel(yproj_kpc_map)[re_large] > 0
+                re_large_negative = np.ravel(yproj_kpc_map)[re_large] < 0
+                poop_positive = np.std(np.ravel(frac)[re_large][re_large_positive])
+                poop_negative = np.std(np.ravel(frac)[re_large][re_large_negative])
+                poop_tmp = (poop_positive + poop_negative) / 2.                         # asymmetry parameter value with bad spaxels
 
-            delta_vel = dap_vel-vel_flip
-            denom = np.sqrt((1/dap_ivar)+(1/ivar_flip))
-            frac = delta_vel/denom
-            flat = np.std(frac)
+                # calculating asymmetry parameter removing the spaxels with s_n(h alpha) < 5
+                delta_vel_g = delta_vel
+                delta_vel_g[snb] = -4000
+                frac_g = delta_vel_g/ denom
+                ha_good_large_positive = np.ravel(s_n_ha)[re_large][re_large_positive] > 5
+                ha_good_large_negative = np.ravel(s_n_ha)[re_large][re_large_negative] > 5
+                poop_positive_good = (np.std(np.ravel(frac)[re_large][re_large_positive][ha_good_large_positive]))
+                poop_negative_good = (np.std(np.ravel(frac)[re_large][re_large_negative][ha_good_large_negative]))
+                poop_tmp_good = (poop_positive_good + poop_negative_good) / 2.          # asymmetry parameter value with bad spaxels removed
+                print(poop_tmp)
 
-            re_arcsec = drpdata.field('nsa_elpetro_th50_r')[match]
-            re_kpc = re_arcsec * u.arcsec / cosmo.arcsec_per_kpc_proper(z)
-            re_large = np.ravel(radkpc) > re_kpc
-            re_large_positive = np.ravel(yproj_kpc_map)[re_large] > 0
-            re_large_negative = np.ravel(yproj_kpc_map)[re_large] < 0
-            poop_positive = np.std(np.ravel(frac)[re_large][re_large_positive])
-            poop_negative = np.std(np.ravel(frac)[re_large][re_large_negative])
-            poop_tmp = (poop_positive + poop_negative) / 2.
-            print(poop_tmp)
-            poop[i] = poop_tmp
+
+                # calculating eta or the velocity dispersion to rotation variable
+                # velocity dispersion
+                ha_good_large = np.ravel(s_n_ha)[re_large] > 5
+                med_vel_disp = np.median(np.ravel(dap_sig)[re_large][ha_good_large])
+                print('the velocity dispersion is', med_vel_disp)
+
+                # velocity roation
+                major = yproj_kpc_map < 1.0  # within 1 kpc of major axis
+                vel_major = (dap_vel)[major]  # might need np.ravel() ?
+                vrot = np.percentile(vel_major, 90)  # 90th percentile make sense?
+                vrot_max = np.round(np.mean(vrot, dtype=None), 5)
+                print('the rotational velocity is', vrot_max)
+                # Vrot/Vdisp
+                med_vel_disp_vrot_max = med_vel_disp / vrot_max
+                print('the velocity dipsersion to rotation ratio is', med_vel_disp_vrot_max)
+
+
+                # plot 1
+                fig = plt.figure()
+                ax = fig.add_subplot(1,2, 1)
+                ax.set_xlim(0, size)
+                ax.set_ylim(0, size)
+                ax.set_yticklabels(())
+                ax.set_xticklabels(())
+                plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+                plt.imshow(frac, origin='lower',
+                           interpolation='nearest',
+                           cmap=cm.coolwarm, vmin=-40, vmax=40)
+                plt.plot(re_kpc)
+                plt.colorbar()
+                plt.text(0,-5,r'$ξ =$'+' '+str(poop_tmp),fontsize=8)
+                plt.title(r'$\frac{v_{gass}-v_{gass,flipped}}{\sqrt{Err(v_{gass})^2 + Err(v_{gass,flipped})^2}}}$', verticalalignment='bottom', fontsize=10)
+                plt.suptitle(good_plates[i], x=0.1, y=0.98)
+
+
+                #  plot 2
+                ax = fig.add_subplot(1, 2, 2)
+                ax.set_xlim(0, size)
+                ax.set_ylim(0, size)
+                ax.set_yticklabels(())
+                plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+                ax.set_xticklabels(())
+                plt.imshow(frac_g, origin='lower',
+                           interpolation='nearest',
+                           cmap=cm.coolwarm, vmin=-40, vmax=40)
+                plt.colorbar()
+                plt.text(20, 95, 's_n h alpha > 5', fontsize=6)
+                plt.title(r'$\frac{v_{gass}-v_{gass,flipped}}{\sqrt{Err(v_{gass})^2 + Err(v_{gass,flipped})^2}}}$', verticalalignment='bottom', fontsize=10)
+                plt.text(0, -5, r'$ξ_{sn>5} =$' + ' ' + str(poop_tmp_good), fontsize=8)
+                plt.text(0, -8, r'$η_{50} =$' + ' ' + str(med_vel_disp_vrot_max), fontsize=8)
+                pdf.savefig()
+                plt.close()
+
+
 
 np.savetxt('poop.txt', poop)
+os.system("open %s &" % filename)
+
+
 
 
 
