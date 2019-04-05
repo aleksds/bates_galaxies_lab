6f3d1a0f7ce39f013ae6b123e3697fad48b23e75
aleksds <aleks.diamond.stanic@gmail.com>
2019-04-05 15:46:04 -0400

    somewhat more complicated version with four panels and three bands, starting to look at how colors change as a function of radius

---
+++ hst/autogalfit/ad_galfit_phot.py
@@ -0,0 +1,194 @@
+# Aleks Diamond-Stanic
+# 20190404
+# Goal: Starting with a suite of suite of output fits files from GALFIT, perform aperture photometry of the data and model to measure total and residual flux
+
+# import relevant packages
+import numpy as np
+import sys
+import os
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from astropy.io import fits
+from photutils import CircularAperture
+from photutils import aperture_photometry
+
+# the code requires the user to specify an input directory that has output fits files from GALFIT
+dir = sys.argv[1]
+
+# find all of the relevant fits files
+files = glob.glob(dir+'/*output.fits')
+
+# define relevant variables
+xcen = 50
+ycen = 50
+fmin = -2
+fmax = 12
+radii = np.arange(40)+1
+
+# arrays for v band
+vdat_flux = np.zeros([len(files),len(radii)])
+vunc_flux = np.zeros([len(files),len(radii)])
+vmod_flux = np.zeros([len(files),len(radii)])
+vres_flux = np.zeros([len(files),len(radii)])
+
+# arrays for u band
+udat_flux = np.zeros([len(files),len(radii)])
+uunc_flux = np.zeros([len(files),len(radii)])
+umod_flux = np.zeros([len(files),len(radii)])
+ures_flux = np.zeros([len(files),len(radii)])
+
+# arrays for j band
+jdat_flux = np.zeros([len(files),len(radii)])
+junc_flux = np.zeros([len(files),len(radii)])
+jmod_flux = np.zeros([len(files),len(radii)])
+jres_flux = np.zeros([len(files),len(radii)])
+
+# let's produce plots that will be saved in a pdf file
+name = 'galfit_phot_'+dir+'.pdf'
+
+with PdfPages(name) as pdf:
+    
+    # loop over each output fits file from GALFIT
+    for i in range(0, len(files)):
+        
+        # read in the image
+        hdu = fits.open(files[i])
+        
+        # these images have 18 different extensions:
+        # hdu[0] is INPUT_V
+        # hdu[1] is INPUT_U
+        # hdu[2] is INPUT_J
+        # hdu[3] is MODEL_V
+        # hdu[4] is MODEL_U
+        # hdu[5] is MODEL_J
+        # hdu[6] is RESIDUAL_V
+        # hdu[7] is RESIDUAL_U
+        # hdu[8] is RESIDUAL_J
+        # hdu[9] is COMPONENT_1_sersic_V
+        # hdu[10] is COMPONENT_1_sersic_U
+        # hdu[11] is COMPONENT_1_sersic_J
+        # hdu[12] is SIGMA_V
+        # hdu[13] is SIMGA_U
+        # hdu[14] is SIGMA_J
+        # hdu[15] is PSF_V
+        # hdu[16] is PSF_U
+        # hdu[17] is PSF_J
+
+        # F814W corresponds roughly to rest-frame V-band
+        vdat, vdat_head = hdu[0].data, hdu[0].header
+        vunc, vunc_head = hdu[12].data, hdu[12].header
+        vmod, vmod_head = hdu[3].data, hdu[3].header
+        vres, vres_head = hdu[6].data, hdu[6].header
+
+        # F475W corresponds roughly to rest-frame U-band
+        udat, udat_head = hdu[1].data, hdu[1].header
+        uunc, uunc_head = hdu[13].data, hdu[13].header
+        umod, umod_head = hdu[4].data, hdu[4].header
+        ures, ures_head = hdu[7].data, hdu[7].header        
+
+        # F160W corresponds roughly to rest-frame J-band
+        jdat, jdat_head = hdu[2].data, hdu[2].header
+        junc, junc_head = hdu[14].data, hdu[14].header
+        jmod, jmod_head = hdu[5].data, hdu[5].header
+        jres, jres_head = hdu[8].data, hdu[8].header  
+        
+        # loop over each aperture
+        for j in range(0,len(radii)):
+
+            # define the circular aperture
+            aperture = CircularAperture([xcen, ycen], radii[j])
+
+            # perform aperture photmetry in that aperture and save the flux value
+            vdat_table = aperture_photometry(vdat, aperture)
+            vdat_flux[i,j] = vdat_table['aperture_sum'][0]
+            udat_table = aperture_photometry(udat, aperture)
+            udat_flux[i,j] = udat_table['aperture_sum'][0]
+            jdat_table = aperture_photometry(jdat, aperture)
+            jdat_flux[i,j] = jdat_table['aperture_sum'][0]
+                        
+            # repeat for the sigma image
+            vunc_table = aperture_photometry(vunc, aperture)
+            vunc_flux[i,j] = vunc_table['aperture_sum'][0]
+            uunc_table = aperture_photometry(uunc, aperture)
+            uunc_flux[i,j] = uunc_table['aperture_sum'][0]
+            junc_table = aperture_photometry(junc, aperture)
+            junc_flux[i,j] = junc_table['aperture_sum'][0]
+                        
+            # repeat for the model image
+            vmod_table = aperture_photometry(vmod, aperture)
+            vmod_flux[i,j] = vmod_table['aperture_sum'][0]
+            umod_table = aperture_photometry(umod, aperture)
+            umod_flux[i,j] = umod_table['aperture_sum'][0]
+            jmod_table = aperture_photometry(jmod, aperture)
+            jmod_flux[i,j] = jmod_table['aperture_sum'][0]
+                        
+            # repeat for the residual image
+            vres_table = aperture_photometry(vres, aperture)
+            vres_flux[i,j] = vres_table['aperture_sum'][0]
+            ures_table = aperture_photometry(ures, aperture)
+            ures_flux[i,j] = ures_table['aperture_sum'][0]
+            jres_table = aperture_photometry(jres, aperture)
+            jres_flux[i,j] = jres_table['aperture_sum'][0]
+              
+        # start our figure
+        fig = plt.figure()
+
+        # have the title at the top of the page be the name of the galfit output 
+        plt.suptitle(vdat_head['LOGFILE'])
+
+        # plot flux vs radius for the data, uncertainty, model, and residual
+        ax = fig.add_subplot(2,2,1)
+        ax.scatter(radii, vdat_flux[i]/1e5)
+        #plt.errorbar(radii,vdat_flux[i]/1e5,yerr=vunc_flux[i]/1e5)
+        ax.scatter(radii, vunc_flux[i]/1e5, marker='s', color='red')
+        ax.scatter(radii, vmod_flux[i]/1e5, marker='o', color='green')
+        ax.scatter(radii, vres_flux[i]/1e5, marker='+', color='black')
+        ax.set_ylim(fmin, fmax)
+
+        # add labels for axes and a title for the name of the image extension
+        #plt.xlabel('Radius [pixels]')#, fontsize=14)
+        plt.ylabel('Flux [image units]')#, fontsize=14)
+        plt.title(vdat_head['EXTNAME'])
+
+        # repeat for F475W
+        ax = fig.add_subplot(2,2,2)
+        ax.scatter(radii, udat_flux[i]/1e5)
+        ax.scatter(radii, uunc_flux[i]/1e5, marker='s', color='red')
+        ax.scatter(radii, umod_flux[i]/1e5, marker='o', color='green')
+        ax.scatter(radii, ures_flux[i]/1e5, marker='+', color='black')
+        ax.set_ylim(fmin, fmax)
+        
+        # add labels for axes and a title for the name of the image extension
+        #plt.xlabel('Radius [pixels]')
+        #plt.ylabel('Flux [image units]')
+        plt.title(udat_head['EXTNAME'])
+
+        # repeat for F160W
+        ax = fig.add_subplot(2,2,3)
+        ax.scatter(radii, jdat_flux[i]/1e5)
+        ax.scatter(radii, junc_flux[i]/1e5, marker='s', color='red')
+        ax.scatter(radii, jmod_flux[i]/1e5, marker='o', color='green')
+        ax.scatter(radii, jres_flux[i]/1e5, marker='+', color='black')
+        ax.set_ylim(fmin, fmax*3)
+        
+        # add labels for axes and a title for the name of the image extension
+        plt.xlabel('Radius [pixels]')
+        plt.ylabel('Flux for '+jdat_head['EXTNAME'])
+        #plt.title(jdat_head['EXTNAME'])
+
+
+        # plot F160W / F814W ratio and F814W / F475W ratio for residual flux
+        ax = fig.add_subplot(2,2,4)
+        ax.scatter(radii, jres_flux[i] / vres_flux[i], marker='+', color='red')
+        ax.scatter(radii, vres_flux[i] / ures_flux[i], marker='+', color='green')
+        ax.set_ylim(-10, 10)
+        plt.xlabel('Radius [pixels]')
+        #plt.ylabel('jres / vres')
+        # save this page of the pdf file
+        pdf.savefig()
+        plt.close()
+
+# open the pdf file
+os.system('open %s &' % name)
+
