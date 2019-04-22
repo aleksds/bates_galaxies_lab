fc7c5a2c24cd55669c84ea131dacc0b5da4bc5e0
King Valdez <kvaldez@bates.edu>
2019-04-22 11:00:57 -0400

    this is my version of finding the best, upper, lower limits to escape velocity

--- hst/PROSPECTOR/kv_vesc_limits.py
+++ hst/PROSPECTOR/kv_vesc_limits.py
@@ -7,21 +7,49 @@ from astropy.cosmology import FlatLambdaCDM
 cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Om0=0.3)
 
 # should read this from a table
-m__ssp_lo = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
-m__ssp_up = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
-m__cal_lo = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
-m__cal_up = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
-m__tau_lo = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
-m__tau_up = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
-m__dtau_lo = np.array([1.1e9, 7.9e9, 2.6e9, 8.2e9, 2.3e9, 2.9e9, 1.0e10, 3.2e9, 7.4e9, 4.7e9, 9.5e9, 1.4e10]) * const.M_sun
+m_ssp_lo = np.array([1.07e10, 6.68e9, 1.06e10, 1.06e10, 7.72e9, 1.07e10, 1.05e10, 1.07e10, 1.06e10, 7.16e9, 1.07e10, 1.45e10]) * const.M_sun #default ssp stellar mass values for 16th percentile
+m_ssp_best = np.array([1.24e10, 8.69e9, 1.23e10, 1.24e10, 1.22e10, 1.24e10, 1.23e10, 1.24e10, 1.24e10, 1.18e10, 1.24e10, 1.82e10]) * const.M_sun #50th percentile
+m_ssp_up = np.array([1.44e10, 1.15e10, 1.43e10, 1.44e10, 1.43e10, 1.44e10, 1.42e10, 1.43e10, 1.44e10, 1.4e10, 1.43e10, 2.23e10]) * const.M_sun #84th percentile
 
-re_hi_arc = np.array([0.033, 0.029, 0.018, 0.023, 0.041, 0.058, 0.021, 0.023, 0.088, 0.177, 0.044, 0.045]) * u.arcsec
+m_cal_lo = np.array([7.16e9, 8.16e9, 2.13e9, 7.64e9, 9.34e9, 1.07e10, 7.79e9, 2.5e9, 7.95e9, 4.45e9, 2.32e9, 1.27e9]) * const.M_sun #calzetti 16th%
+m_cal_best = np.array([1.17e10, 1.45e10, 2.68e9, 1.26e10, 1.47e10, 1.16e10, 1.31e10, 1.22e10, 1.06e10, 3.97e10, 9.29e9, 1.25e10]) * const.M_sun #50th percentile
+m_cal_up = np.array([1.37e10, 1.77e10, 1.08e10, 1.66e10, 1.76e10, 1.25e10, 1.67e10, 1.5e10, 1.22e10, 5.45e10, 1.38e10, 1.91e10]) * const.M_sun #calzetti 84th%
+
+m_tau_lo = np.array([4.26e9, 4.33e9, 4.28e9, 4.25e9, 4.32e9, 4.34e9, 4.22e9, 4.3e9, 4.79e9, 4.27e9, 4.33e9, 4.33e9]) * const.M_sun #tau 16th%
+m_tau_best = np.array([5.56e9, 5.92e9, 5.79e9, 5.53e9, 5.73e9, 6.04e9, 5.38e9, 5.66e9, 6.41e9, 5.6e9, 5.8e9, 5.76e9]) * const.M_sun #50th percentile
+m_tau_up = np.array([9.11e9, 9.26e9, 9.26e9, 8.9e9, 9.08e9, 9.36e9, 8.89e9, 9.14e9, 9.51e9, 9.16e9, 9.3e9, 9.23e9]) * const.M_sun #tau 84th%
+
+m_dtau_lo = np.array([4.99e9, 5.03e9, 5.02e9, 5.07e9, 5.04e9, 5.08e9, 5.07e9, 5.06e9, 5.49e9, 5.04e9, 5.48e9, 7.58e9]) * const.M_sun #dtau 16th%
+m_dtau_best = np.array([6.04e9, 6.04e9, 6.08e9, 6.12e9, 6.07e9, 6.14e9, 6.15e9, 6.07e9, 6.6e9, 6.09e9, 6.6e9, 9.79e9]) * const.M_sun #50th percentile
+m_dtau_up = np.array([7.73e8, 7.70e9, 7.73e9, 7.94e9, 7.74e9, 8e9, 8.01e9, 7.74e9, 8.31e9, 7.73e9, 8.26e9, 1.21e10]) * const.M_sun #dtau 84th%
+
+re_hi_arc = np.array([0.727, 0.883, 0.434, 0.575, 1.048, 1.392, 0.392, 0.610, 2.483, 6.386, 1.057, 0.949]) * u.arcsec
+re_lo_arc = np.array([0.476, 0.584, 0.400, 0.346, 0.465, 0.904, 0.568, 0.392, 0.959, 4.048, 0.628, 0.003]) * u.arcsec
+re_best_arc = np.array([0.610, 0.697, 0.424, 0.394, 0.650, 1.187, 0.549, 0.469, 1.385, 5.310, 0.918, 0.509]) * u.arcsec
 z = np.array([0.603, 0.459, 0.712, 0.514, 0.467, 0.451, 0.658, 0.608, 0.402, 0.449, 0.728, 0.752])
 galaxies = ['J0826', 'J0901', 'J0905', 'J0944', 'J1107', 'J1219', 'J1341', 'J1506', 'J1558', 'J1613', 'J2116', 'J2140']
 
 
 for i in range(0, len(z)):
     re_hi_kpc = re_hi_arc[i] / cosmo.arcsec_per_kpc_proper(z[i])
-    vesc_lo = np.sqrt(G * mass_lo[i] / re_hi_kpc).to('km/s')
-    print(galaxies[i], re_hi_kpc, vesc_lo)
+    re_lo_kpc = re_hi_arc[i] / cosmo.arcsec_per_kpc_proper(z[i])
+    re_best_kpc = re_hi_arc[i] / cosmo.arcsec_per_kpc_proper(z[i])
+
+    vesc_ssp_lo = np.sqrt(G * m_ssp_lo[i] / re_hi_kpc).to('km/s')
+    vesc_ssp_best = np.sqrt(G * m_ssp_best[i] / re_hi_kpc).to('km/s')
+    vesc_ssp_up = np.sqrt(G * m_ssp_up[i] / re_hi_kpc).to('km/s')
+
+    vesc_cal_lo = np.sqrt(G * m_cal_lo[i] / re_hi_kpc).to('km/s')
+    vesc_cal_best = np.sqrt(G * m_cal_best[i] / re_hi_kpc).to('km/s')
+    vesc_cal_up = np.sqrt(G * m_cal_up[i] / re_hi_kpc).to('km/s')
+
+    vesc_tau_lo = np.sqrt(G * m_tau_lo[i] / re_hi_kpc).to('km/s')
+    vesc_tau_best = np.sqrt(G * m_tau_best[i] / re_hi_kpc).to('km/s')
+    vesc_tau_up = np.sqrt(G * m_tau_up[i] / re_hi_kpc).to('km/s')
+
+    vesc_dtau_lo = np.sqrt(G * m_dtau_lo[i] / re_hi_kpc).to('km/s')
+    vesc_dtau_best = np.sqrt(G * m_dtau_best[i] / re_hi_kpc).to('km/s')
+    vesc_dtau_up = np.sqrt(G * m_dtau_up[i] / re_hi_kpc).to('km/s')
+    print(galaxies[i], re_hi_kpc, vesc_ssp_lo,vesc_ssp_best,vesc_ssp_up,vesc_cal_lo,vesc_cal_best,
+          vesc_cal_up,vesc_tau_lo,vesc_tau_best,vesc_tau_up,vesc_dtau_lo, vesc_dtau_best,vesc_dtau_up)
 
