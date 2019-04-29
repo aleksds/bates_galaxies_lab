a5820fcaee4ec1cb2db0529a20786d6b4e716803
aleksds <aleks.diamond.stanic@gmail.com>
2019-04-29 09:48:50 -0400

    updated version that demonstrates agreement between chi^2 calculations

--- hst/autogalfit/read_chi_part2.py
+++ hst/autogalfit/read_chi_part2.py
@@ -15,25 +15,43 @@ from photutils import CircularAperture
 from photutils import aperture_photometry
 
 dir = sys.argv[1]
-files = glob.glob(dir+'*.band')
-files = glob.glob(dir+'/*output.fits')
+files = glob.glob(dir+'/*.band')
+images = glob.glob(dir+'/*output.fits')
 chi = np.zeros(len(files))
 file = "All_the_Chi_Values.dat"
 file_object = open('All_the_Chi_Values.dat','a+')
-file_object.write("Chi Values From Galfit\n")
+#file_object.write("Chi Values From Galfit\n")
+chi_from_our_calculations = np.zeros(len(files))
+chi = np.zeros(len(files))
 for i in range (0, len(files)):
+    print(images[i])
+    hdu = fits.open(images[i])
+    # F814W corresponds roughly to rest-frame V-band
+    vdat, vdat_head = hdu[0].data, hdu[0].header
+    vunc, vunc_head = hdu[12].data, hdu[12].header
+    vmod, vmod_head = hdu[3].data, hdu[3].header
+    vres, vres_head = hdu[6].data, hdu[6].header
+
+    # F475W corresponds roughly to rest-frame U-band
+    udat, udat_head = hdu[1].data, hdu[1].header
+    uunc, uunc_head = hdu[13].data, hdu[13].header
+    umod, umod_head = hdu[4].data, hdu[4].header
+    ures, ures_head = hdu[7].data, hdu[7].header        
+
+    # F160W corresponds roughly to rest-frame J-band
+    jdat, jdat_head = hdu[2].data, hdu[2].header
+    junc, junc_head = hdu[14].data, hdu[14].header
+    jmod, jmod_head = hdu[5].data, hdu[5].header
+    jres, jres_head = hdu[8].data, hdu[8].header  
+    chi_from_our_calculations[i] = np.sum((vres/vunc)**2) + np.sum((ures/uunc)**2) + np.sum((jres/junc)**2)
     print(files[i])
     with open(files[i]) as f:
         content = f.readlines()
-        print(content[3][15:19])
-        chi[i] = np.float(content[3][15:19])
-        vdat, vadt_head = raw_data[0].data, raw_data[0].header
-        vunc, vunc_head = raw_data[12].data, raw_data[12].header
-        vmod, vmod_head = raw_data[3].data, raw_data[3].header
-        vres, vres_head = raw_data[6].data, raw_data[6].header
-        chi_from_our_calculations[i] = np.sum((vres/vunc)**2)/((101**2*3)
-        print(chi_from_our_calculations[i])
-        file_object.write(str(chi[i])+ "" +str(chi_from_our_calculations[i]) '\n')
+        #print(content[3][15:19])
+        print(content[3][31:41])
+        chi[i] = np.float(content[3][31:41])
+    print(chi_from_our_calculations[i])
+    file_object.write(files[i]+' '+str(chi[i])+" "+str(chi_from_our_calculations[i])+'\n')
 file_object.close
 # Up to this part, it creates a code which works and produces all of the chi values in a colum
 # and a name is printed out for each of the data that are given by the galfit file. 
