eae349529b865d6f76f6ac5c9a0de3527f5e250b
King Valdez <kvaldez@bates.edu>
2019-06-05 23:28:18 -0400

    new fits files and my code which extracts mag and uncertainties and updated mag and uncertainty table

--- hst/kv_wise_match.py
+++ hst/kv_wise_match.py
@@ -13,29 +13,15 @@ galaxies = ['J0826', 'J0901', 'J0905', 'J0944', 'J1107', 'J1219', 'J1341', 'J150
 for i in range(0, len(galaxies)):
 
     files = glob.glob('J*.fits')
-    print(galaxies[i])
     hdu = fits.open(files[i])
-    print(hdu)
-
     data = hdu[1].data
-    print('data:',data)
-
     w1_mag = hdu[1].data.field('w1_mag')
-    print('w1_mag',w1_mag)
     w1_mag_err = hdu[1].data.field('w1_mag_err')
-    print('w1_mag_err',w1_mag_err)
-
     w2_mag = hdu[1].data.field('w2_mag')
-    print('w2_mag', w2_mag)
     w2_mag_err = hdu[1].data.field('w2_mag_err')
-    print('w2_mag_err',w2_mag_err)
-
     w3_mag = hdu[1].data.field('w3_mag')
-    print('w3_mag', w3_mag)
     w3_mag_err = hdu[1].data.field('w3_mag_err')
-    print('w3_mag_err',w2_mag_err)
-
     w4_mag = hdu[1].data.field('w4_mag')
-    print('w4_mag',w4_mag)
     w4_mag_err = hdu[1].data.field('w4_mag_err')
-    print('w4_mag_err',w4_mag_err)
+    print(galaxies[i],'w1_mag',w1_mag,'w1_mag_err',w1_mag_err,'w2_mag', w2_mag,'w2_mag_err',w2_mag_err,
+          'w3_mag',w3_mag,'w3_mag_err',w3_mag_err,'w4_mag',w4_mag,'w4_mag_err',w4_mag_err)
