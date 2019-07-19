1da31b47ba8ea622c816781a538d37b95a0c2b6b
Aleksandar Diamond-Stanic <aleks.diamond.stanic@gmail.com>
2019-07-19 13:27:50 -0400

    adding additional scripts

---
+++ hst/PROSPECTOR/adsdir/nuclear_bursty_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J2140' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J2116' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J1613' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J1558' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J1506' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J1341' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J1219' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J1107' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J0944' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J0905' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J0901' --sedfit  --verbose 
+python bgl_sedfit.py --nproc=4 --priors='bursty' --galaxy='J0826' --sedfit  --verbose 
+
