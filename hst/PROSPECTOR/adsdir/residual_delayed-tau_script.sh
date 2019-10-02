a3b24f3640175af0c0b51fb7f1e2e68916694658
Aleksandar Diamond-Stanic <aleks.diamond.stanic@gmail.com>
2019-10-02 09:41:58 -0400

    new code to do SED fitting of residual / extended photometry

---
+++ hst/PROSPECTOR/adsdir/residual_delayed-tau_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J2140' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J2116' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J1613' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J1558' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J1506' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J1341' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J1219' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J1107' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J0944' --sedfit  --verbose 
+python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J0905' --sedfit  --verbose 
+#python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J0901' --sedfit  --verbose 
+python bgl_resfit.py --nproc=4 --priors='delayed-tau' --galaxy='J0826' --sedfit  --verbose 
+
