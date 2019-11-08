08bf8a41364bf21c1910c077ab77b4be225daa11
Aleksandar Diamond-Stanic <aleks.diamond.stanic@gmail.com>
2019-11-07 22:05:43 -0500

    update to include nuclear UVJ colors

--- hst/autogalfit/uvj_plot.py
+++ hst/autogalfit/uvj_plot.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from astropy.io import ascii
 
-data = ascii.read('bgl_uvj_ext.dat')
+extdata = ascii.read('bgl_uvj_ext.dat')
+nucdata = ascii.read('bgl_uvj_nuc.dat')
 
 filename = 'uvj_plot.pdf'
 
@@ -12,27 +13,38 @@ with PdfPages(filename) as pdf:
     
     fig = plt.figure()
 
-    uv_color = data['umag']-data['vmag']
-    vj_color = data['vmag']-data['jmag']
+    ext_uv_color = extdata['umag']-extdata['vmag']
+    ext_vj_color = extdata['vmag']-extdata['jmag']
 
-    uv_err = np.sqrt(data['uerr']**2+data['verr']**2)
-    vj_err = np.sqrt(data['verr']**2+data['jerr']**2)
+    nuc_uv_color = nucdata['umag']-nucdata['vmag']
+    nuc_vj_color = nucdata['vmag']-nucdata['jmag']
     
-    plt.scatter(vj_color, uv_color)
-    plt.errorbar(vj_color, uv_color, xerr=vj_err, yerr=uv_err, fmt='o', elinewidth=1)
+    ext_uv_err = np.sqrt(extdata['uerr']**2+extdata['verr']**2)
+    ext_vj_err = np.sqrt(extdata['verr']**2+extdata['jerr']**2)
 
+    nuc_uv_err = np.sqrt(nucdata['uerr']**2+nucdata['verr']**2)
+    nuc_vj_err = np.sqrt(nucdata['verr']**2+nucdata['jerr']**2)
+    
+    #plt.scatter(ext_vj_color, ext_uv_color, color='red')
+    plt.errorbar(ext_vj_color, ext_uv_color, xerr=ext_vj_err, yerr=ext_uv_err, fmt='o', marker='.', elinewidth=1, color='red', label='extended')
+
+    #plt.scatter(nuc_vj_color, nuc_uv_color, color='blue', marker='*')
+    plt.errorbar(nuc_vj_color, nuc_uv_color, xerr=nuc_vj_err, yerr=nuc_uv_err, fmt='o', marker='*', elinewidth=1, color='blue', label='central')
+    
     plt.xlabel('V-J')
     plt.ylabel('U-V')
     
-    plt.xlim([0,2.5])
+    plt.xlim([-0.5,2.0])
     plt.ylim([0,2.5])
 
+    plt.legend()
+    
     # 0.807 - 1.998
     slant_vj = np.arange(1600-807+1)/1000+0.807
     slant_uv = 0.88 * slant_vj + 0.59
     plt.plot(slant_vj, slant_uv, color='black')
 
-    horiz_vj = np.arange(807+1)/1000.
+    horiz_vj = np.arange(500.+807+1)/1000.-0.5
     horiz_uv = np.zeros(len(horiz_vj))+1.3
     plt.plot(horiz_vj, horiz_uv, color='black')
 
@@ -41,8 +53,9 @@ with PdfPages(filename) as pdf:
     plt.plot(vert_vj, vert_uv, color='black')
 
     
-    #for i in range(0, len(data)):
-    #    plt.text(vj_color[i], uv_color[i], data['Galaxy'][i])
+    #for i in range(0, len(extdata)):
+    #    plt.text(ext_vj_color[i], ext_uv_color[i], extdata['Galaxy'][i])
+    #    plt.text(nuc_vj_color[i], nuc_uv_color[i], nucdata['Galaxy'][i])
     
     pdf.savefig()
     plt.close()
