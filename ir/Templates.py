4649c58f5bcf42d9000cd4a5cdfda0386974208c
Aleksandar Diamond-Stanic <aleks.diamond.stanic@gmail.com>
2020-10-06 07:42:01 -0400

    tweaks to code from Grayson Petter

---
+++ ir/Templates.py
@@ -0,0 +1,310 @@
+#
+# Author: Grayson Petter
+# Templates.py
+# Functions to redshift, interpolate, and integrate IR templates in order to simulate observed colors & luminosities
+
+import numpy as np
+from astropy import units as u
+import math
+import pickle
+import WISE
+from astropy.cosmology import FlatLambdaCDM
+import pandas as pd
+import glob
+import importlib
+import os
+importlib.reload(WISE)
+#reload(WISE)
+
+# speed of light
+c = 299792458.  # m/s
+
+# path to project
+
+projpath = os.getcwd()+'/' #'../'#'/Users/graysonpetter/Desktop/IRSFRs/'
+
+# set cosmology for calculating luminosity distance
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+# Read in all templates except for AGN (SFGs and Composite)
+templates = glob.glob(projpath + 'Comprehensive_library/SFG*.txt')
+templates.extend(
+    glob.glob(projpath + 'Comprehensive_library/Comp*.txt'))
+
+print('templates:', templates)
+
+# read in the W3 & W4 bandpasses
+wise_bandpasses_3_4 = sorted(glob.glob(projpath + 'bandpass/*.txt'))[2:4]
+
+
+# redshift a template SED (wavelengths only)
+def redshift_spectrum(z, template, trim):
+
+    t = pd.read_csv(template, delim_whitespace=True, engine='python', header=None, skiprows=3)
+
+    # read off wavelengths and luminosities from template
+    wavelengths = np.array(t.iloc[:, 0])
+    wavelengths = wavelengths.astype(float)
+    #print('wavelengths: ', wavelengths)
+    Lums = np.array(t.iloc[:, 1])
+
+    # cut template down to 8-1000 microns (TIR) definition
+    if trim:
+        spec_range = np.where((wavelengths >= 8.) & (wavelengths <= 1000.))[0]
+        wavelengths = wavelengths[spec_range]
+        Lums = Lums[spec_range]
+
+    # redshift wavelengths
+    shifted_len = np.array(wavelengths)*(1+z)
+
+    # get luminosity at 12 & 22 micron in observed frame
+    twelve_mu = (np.abs(shifted_len - 12)).argmin()
+    twenty_two_mu = (np.abs(shifted_len - 22)).argmin()
+
+    return wavelengths, Lums, Lums[twelve_mu], Lums[twenty_two_mu], shifted_len
+
+
+# linearly interpolate the SED in frequency space to make integration simple
+def interpolate_spec(shifted_spec, model):
+    # convert wavelengths in microns to frequencies in Hz, 10**6 converts microns to meters
+    nus = (10 ** 6) * c / (shifted_spec[0])
+
+    # reverse lists so frequencies go from low to high for simplicity
+    reversed_nus = np.flipud(nus).flatten()
+    # also reverse luminosities
+    reversed_lums = np.flipud(shifted_spec[1])
+
+    # calculate constant frequency interval to interpolate on
+    if model:
+        step = reversed_nus[1] - reversed_nus[0]
+        dx = round(step, -(len(str(int(step))) - 1))
+    else:
+        dx = 10000000000
+
+    # find smallest factor of dx Hz greater than the smallest frequency in the list to start the interpolation
+    start = (reversed_nus[0] + int(dx)) - (reversed_nus[0] % int(dx))
+
+    # range of frequency across entire template
+    span = reversed_nus[len(reversed_nus) - 1] - reversed_nus[0]
+    # number of frequency intervals to interpolate on
+    chunks = int(math.floor(span / dx))
+
+    # lists for interpolated values
+    new_nus, new_lums = [], []
+    current_nu = start
+
+    # linearly interpolate to frequencies in dx Hz steps
+    for x in range(chunks):
+        new_nus.append(current_nu)
+        new_lums.append(np.interp(current_nu, reversed_nus, reversed_lums))
+        current_nu += dx
+
+    return new_nus, new_lums
+
+
+# integrate spectrum using trapezoidal method
+def integrate_spectrum(freqs, Ls):
+    return np.trapz(y=Ls, x=freqs)
+
+
+
+def simulate_wise_fluxes_for_colors(z, tems, bands, csv):
+    tot_mag_list, template_names = [], []
+    # iterate through templates
+    for tem in tems:
+        # redshift template
+        red_spec = redshift_spectrum(z, tem, False)
+        red_waves = np.array(red_spec[4])
+        lumi = np.array(red_spec[1])
+
+        normalized = []
+
+        # iterate through WISE bands
+        for y in range(len(bands)):
+            if csv:
+                band = pd.read_csv(bands[y], header=None, engine='python')
+            else:
+                band = pd.read_csv(bands[y], header=None, delim_whitespace=True, engine='python')
+            bandwaves = np.array(band.iloc[:, 0])
+            band_response = np.array(band.iloc[:, 1])
+
+
+            # trim template to same wavelength range as WISE band
+            cut = np.where((red_waves >= np.min(bandwaves)) & (red_waves <= np.max(bandwaves)))[0]
+            trimmed_y = red_waves[cut]
+            trimmed_L = lumi[cut]
+
+            # interpolate template to band wavelengths, multiply by the response at that wavelength
+            inter_lum = []
+            for j in range(len(bandwaves)):
+                inter_lum.append(band_response[j] * (np.interp(bandwaves[j], trimmed_y, trimmed_L)))
+
+            # crude method
+            """sum_lum = np.sum(np.array(inter_lum))
+            sum_waves = np.sum(np.array(band_response))
+            normalized.append(sum_lum/sum_waves)"""
+
+            # integrate template multiplied by response function
+            spectrum = [bandwaves, inter_lum]
+            interped_again = interpolate_spec(spectrum, True)
+            wise_lums = integrate_spectrum(interped_again[0], interped_again[1])
+
+            # integrate wise band
+            band_spectrum = [bandwaves, band_response]
+            interped_band = interpolate_spec(band_spectrum, True)
+            integrated_band = integrate_spectrum(interped_band[0], interped_band[1])
+            # divide two
+            normalized.append(wise_lums / integrated_band)
+
+        tot_mag_list.append(normalized)
+
+        template_names.append(tem.split('.txt')[0].split('/')[8])
+
+    return tot_mag_list, template_names
+
+
+# simulate observed WISE fluxes by integrating templates over WISE bandpasses
+def simulate_wise_fluxes(z, tem, bands, csv):
+
+    # redshift template
+    red_spec = redshift_spectrum(z, tem, False)
+
+    shifted_wavelengths = np.array(red_spec[4])
+    lumi = np.array(red_spec[1])
+
+    normalized = []
+
+    # iterate through WISE bands
+    for y in range(len(bands)):
+        if csv:
+            band = pd.read_csv(bands[y], header=None, engine='python')
+        else:
+            band = pd.read_csv(bands[y], header=None, delim_whitespace=True, engine='python')
+        # bandpass wavelength list
+        bandwaves = np.array(band.iloc[:, 0])
+        # bandpass response function values
+        band_response = np.array(band.iloc[:, 1])
+        # convolve wavelength with response function per Greg Rudnick's suggestion to account for the fact
+        # that the WISE detectors are photon counting devices, while the templates are energy templates
+        band_convolved = np.multiply(bandwaves, band_response)
+
+        # trim template to same wavelength range as WISE band
+        cut = np.where((shifted_wavelengths >= np.min(bandwaves)) & (shifted_wavelengths <= np.max(bandwaves)))[0]
+        trimmed_y = shifted_wavelengths[cut]
+        trimmed_L = lumi[cut]
+
+        # interpolate template to wavelengths in the bandpass list, multiply by the convolved response function
+        # at that wavelength
+        inter_lum = []
+        for i in range(len(bandwaves)):
+            inter_lum.append(band_convolved[i] * (np.interp(bandwaves[i], trimmed_y, trimmed_L)))
+
+        # integrate template multiplied by response function
+        spectrum = [bandwaves, inter_lum]
+        interped_again = interpolate_spec(spectrum, True)
+        wise_lums = integrate_spectrum(interped_again[0], interped_again[1])
+
+        # integrate wise response function to divide out
+        band_spectrum = [bandwaves, band_convolved]
+        interped_band = interpolate_spec(band_spectrum, True)
+        integrated_band = integrate_spectrum(interped_band[0], interped_band[1])
+        # divide two
+        normalized.append(wise_lums / integrated_band)
+    return normalized
+
+
+# integrate the IR templates and write out the total IR luminosity so it can be recalled quickly without doing an
+# integration each time
+def writetotals():
+    totlist = []
+    # for each template
+    for x in range(len(templates)):
+        # call redshift function, but don't actually redshift, just trim to 8-1000 microns
+        shifted_spectrum = redshift_spectrum(0, templates[x], True)
+        # interpolate the template
+        interped_spectrum = interpolate_spec(shifted_spectrum, False)
+        # integrate template from 8-1000 micron
+        total_ir = integrate_spectrum(interped_spectrum[0], interped_spectrum[1])
+        totlist.append(total_ir)
+    # write out the integral totals in a file
+    with open(projpath + 'integrations/kirk.txt', 'wb') as fb:
+        pickle.dump(totlist, fb)
+writetotals()
+
+
+# calculate SFRs using calibration given in Murphy+11
+def murphyIRSFR(L_IR):
+    L_IR = L_IR.to('erg/s').value
+    SFR = 3.88e-44 * L_IR
+    return SFR
+
+
+# calculate IR SFRs
+def IR_SFRs(z, name, tems=templates):
+
+    # luminosity distance
+    d = cosmo.luminosity_distance(z)
+    # convert WISE mag to flux in Janskys
+    fluxes = WISE.mag_to_flux(name)
+    w3_flux = fluxes[0] * u.Jy
+    w3_flux_err = fluxes[2] * u.Jy
+    print('W3:', w3_flux, w3_flux_err)
+    # assume no W4 data for now
+    w_four_good = False
+
+    # calculate luminosities with fluxes & distances
+    w3_lum = (w3_flux*4*np.pi*d**2).to('W/Hz')
+    w3_lum_err = ((4*np.pi*d**2)*w3_flux_err).to('W/Hz')
+
+    # if there's data for W4
+    if not np.isnan(fluxes[1]):
+        w4_flux = fluxes[1] * u.Jy
+        w4_flux_err = fluxes[3] * u.Jy
+        print('W4:', w4_flux, w4_flux_err)
+        w_four_good = True
+        w4_lum = (w4_flux*4*np.pi*d**2).to('W/Hz')
+        w4_lum_err = ((4*np.pi*d**2)*w4_flux_err).to('W/Hz')
+
+    # lists for SFR results
+    SFRs = []
+
+    # read in template total IR luminosities previously calculated
+    with open(projpath + 'integrations/kirk.txt', 'rb') as fb:
+        total_ir = np.array(pickle.load(fb))
+
+    print('tems', tems)
+    # for each template
+    for i, tem in enumerate(tems):
+        print('tem: ', tem)
+        # redshift wavelengths of template
+        tem_lum = redshift_spectrum(z, tem, False)
+
+        # if there is W4 data, do least squares fit of W3 & W4 points to the template curve
+        if w_four_good:
+            # join W3 & W4 observed luminosities
+            measured_lums = np.array([float(w3_lum.value), float(w4_lum.value)])
+            measured_lum_errs = np.array([float(w3_lum_err.value), float(w4_lum_err.value)])
+
+            # simulate a WISE flux by integrating the template over the response curves
+            simulated = np.array(simulate_wise_fluxes(z, tem, wise_bandpasses_3_4, False))
+            print('simulated WISE flux:', simulated)
+
+            # perform least squares fit of observed W3, W4 luminosities to the simulated W3, W4 luminosities
+            # this gives a normalization parameter which can be multiplied by the template TIR luminosity to give an
+            # estimate of the intrinsic luminosity of the source
+            l_ratio = (measured_lums[0]*simulated[0]/(measured_lum_errs[0])**2 + measured_lums[1]*simulated[1]/(measured_lum_errs[1])**2)/((simulated[0]/measured_lum_errs[0])**2 + (simulated[1]/measured_lum_errs[1])**2)
+
+        # if there is no W4 data, simply take ratio of template and observed luminosity at W3
+        else:
+            l_ratio = float(w3_lum.value/tem_lum[2])
+
+        # the observed LIR is just the template TIR luminosity multiplied by the normalization factor determined
+        L_ir_tot = total_ir[i]*l_ratio*u.W
+
+        SFR = murphyIRSFR(L_ir_tot)
+        print(tem, SFR)
+        SFRs.append(SFR)
+
+    return np.average(SFRs), np.std(SFRs)
+
+
---
+++ ir/WISE.py
@@ -0,0 +1,47 @@
+#
+# Author: Grayson Petter
+# WISE.py
+# Takes in a name of a galaxy and reads in the associated WISE fits file, calculates a flux in Jy and colors
+
+import numpy as np
+from astropy.table import Table
+import os
+
+
+#projpath = '/Users/graysonpetter/Desktop/IRSFRs/'
+projpath = os.getcwd()+'/'
+
+# convert from WISE magnitudes to fluxes in Jy
+def mag_to_flux(name):
+
+	WISEdir = projpath + 'unWISE/%s.fits' % name
+	t = Table.read(WISEdir)
+
+	# constants given at http://wise2.ipac.caltech.edu/docs/release/allsky/expsup/sec4_4h.html#example
+	w_three_const = 31.674
+	w_four_const = 8.363
+	# calculate W3, W4 fluxes in Jy
+	flux_three = w_three_const * (10 ** (-(float(t['w3_mag']) / 2.5)))
+	flux_four = w_four_const * (10 ** (-(float(t['w4_mag']) / 2.5)))
+	# errors computed by error propagation formula
+	flux_three_err = w_three_const*np.log(10)/2.5*(10**(-(float(t['w3_mag']) / 2.5)))*float(t['w3_mag_err'])
+	flux_four_err = w_four_const*np.log(10)/2.5*(10**(-(float(t['w4_mag']) / 2.5)))*float(t['w4_mag_err'])
+
+	return flux_three, flux_four, flux_three_err, flux_four_err
+
+
+# calculate WISE colors and errors
+def colors(name):
+	pat = projpath + 'unWISE/%s.fits' % name
+	t = Table.read(pat)
+	# W1-W2
+	one_two = float(t['w1_mag'])-float(t['w2_mag'])
+	one_two_err = np.sqrt((float(t['w1_mag_err']))**2+(float(t['w2_mag_err']))**2)
+	# W3-W4
+	three_four = float(t['w3_mag'])-float(t['w4_mag'])
+	three_four_err = np.sqrt((float(t['w3_mag_err'])) ** 2 + (float(t['w4_mag_err'])) ** 2)
+	# W2-W3
+	two_three = float(t['w2_mag'])-float(t['w3_mag'])
+	two_three_err = np.sqrt((float(t['w2_mag_err'])) ** 2 + (float(t['w3_mag_err'])) ** 2)
+	return one_two, three_four, two_three, one_two_err, three_four_err, two_three_err
+
